402b19beded383ac49a20dd72f8094e59aefa93b
jerng-machines <sbx_user1051@169.254.22.157>
2020-05-22 19:45:23 +0000

    A MESSAGE

--- tasks/restfulTasks/getAllSchemas.js
+++ tasks/restfulTasks/getAllSchemas.js
@@ -1,16 +1,11 @@
 'use strict'
 
-const mark      = require ( '../../modules/mark' )            
-const DDBDC     = require ( '../../io/DDBDC.js' )
+const mark      = require ( '/var/task/modules/mark' )            
+const DDBDC     = require ( '/var/task/io/DDBDC.js' )
 
 const getAllSchemasTask = async ( data ) => {
     
-    data.RU.io.gridSchemasScan = await DDBDC.scan ( {
-        TableName: 'TEST-APP-GRID-SCHEMAS',
-        ReturnConsumedCapacity : 'TOTAL'
-    } ).promise()
-    
-    data.RU.response.markupName = 'allSchemasMarkup'
+  
 
     mark ( `getAllSchemas.js EXECUTED` )
 }
