1dc780f54cee43961961ebbb9e9e628e3c0fa22f
jerng-machines <sbx_user1051@169.254.61.213>
2020-05-22 18:23:46 +0000

    ?ruthenium=restful&type=schemas ... this now works; file naming and require(path) conventions ugly; FIX NEXT!

--- modules/gitCommit.js
+++ modules/gitCommit.js
@@ -83,7 +83,7 @@ const gitCommit = () => { try {
         { encoding: 'utf8', cwd: '/tmp/ruthenium' }).split('\n')
         
     notes.gitCommmit = childProcess.execSync(
-        'git -c user.name=jerng-machines commit -m "A MESSAGE" ', 
+        'git -c user.name=jerng-machines commit -m "?ruthenium=restful&type=schemas ... this now works; file naming and require(path) conventions ugly; FIX NEXT!" ', 
         { encoding: 'utf8', cwd: '/tmp/ruthenium' }).split('\n')
 
     notes.gitPush = childProcess.execSync(
