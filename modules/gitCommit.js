b9a3e79f62afbcc908d96fa0bffc76b50528e673
jerng-machines <sbx_user1051@169.254.21.37>
2020-05-23 18:59:25 +0000

    lastGuard.js refactored to burp (data) on errors

--- modules/gitCommit.js
+++ modules/gitCommit.js
@@ -29,7 +29,7 @@ Environmental variable with password
 const mark          = require ( './mark' )            
 const childProcess  = require('child_process')
 
-const commitMessage = `A MESSAGE`
+const commitMessage = `lastGuard.js refactored to burp (data) on errors`
 
 const gitCommit = () => { try {
 
