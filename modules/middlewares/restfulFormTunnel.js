1dabf3438fc0881b80b912319220563ee2477a04
jerng-machines <sbx_user1051@169.254.110.117>
2020-05-23 19:39:02 +0000

    middleware::restfulFormTunnel.js introduced, but not implemented

--- modules/middlewares/restfulFormTunnel.js
+++ modules/middlewares/restfulFormTunnel.js
@@ -3,7 +3,7 @@
 //  Addresses this issue by using a conventional tunnel approach: 
 //  https://softwareengineering.stackexchange.com/questions/114156/why-are-there-are-no-put-and-delete-methods-on-html-forms
 
-const mark      = require ( '../modules/mark' )            
+const mark      = require ( '/var/task/modules/mark' )            
 
 //  Provide a debuggable function name, 
 //  in order to avoid debuggin (function).toString()
