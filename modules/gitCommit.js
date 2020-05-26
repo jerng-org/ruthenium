e2df21b082e1208b8e5cd047aa6ea4276eed8646
jerng-machines <sbx_user1051@169.254.87.189>
2020-05-26 20:32:56 +0000

    formData normalised; markup now async

--- modules/gitCommit.js
+++ modules/gitCommit.js
@@ -29,7 +29,7 @@ Environmental variable with password
 const mark          = require ( './mark' )            
 const childProcess  = require('child_process')
 
-const commitMessage = `-`
+const commitMessage = `formData normalised; markup now async`
 
 const gitCommit = () => { try {
 
