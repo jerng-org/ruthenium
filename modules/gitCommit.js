597ccd40dab843cc3111276794a87f1311d12870
jerng-machines <sbx_user1051@169.254.137.125>
2020-05-27 13:32:37 +0000

    -

--- modules/gitCommit.js
+++ modules/gitCommit.js
@@ -29,7 +29,7 @@ Environmental variable with password
 const mark          = require ( './mark' )            
 const childProcess  = require('child_process')
 
-const commitMessage = `Work on decoupling RU code from LAMBDA.`
+const commitMessage = `-`
 
 const gitCommit = () => { try {
 
