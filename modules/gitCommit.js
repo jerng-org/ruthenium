b78ce05a0ed301b059a94760ebfd799e5505f065
jerng-machines <sbx_user1051@169.254.248.205>
2020-05-27 22:03:48 +0000

    increased filetree proximity of vertically integrated tasks-markups

--- modules/gitCommit.js
+++ modules/gitCommit.js
@@ -29,7 +29,7 @@ Environmental variable with password
 const mark          = require ( './mark' )            
 const childProcess  = require('child_process')
 
-const commitMessage = `-`
+const commitMessage = `increased filetree proximity of vertically integrated tasks-markups `
 
 const gitCommit = () => { try {
 
