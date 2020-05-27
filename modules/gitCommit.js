d1a7c0fa879ed6558e13570f74541837699c8657
jerng-machines <sbx_user1051@169.254.62.181>
2020-05-27 13:40:40 +0000

    removed Lambda-specific code from ruthenium.js

--- modules/gitCommit.js
+++ modules/gitCommit.js
@@ -29,7 +29,7 @@ Environmental variable with password
 const mark          = require ( './mark' )            
 const childProcess  = require('child_process')
 
-const commitMessage = `-`
+const commitMessage = `removed Lambda-specific code from ruthenium.js`
 
 const gitCommit = () => { try {
 
