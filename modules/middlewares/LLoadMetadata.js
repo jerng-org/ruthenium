97e84cd6b5ac31ea9241e3337083606ff8bfdc08
jerng-machines <sbx_user1051@169.254.63.181>
2020-05-27 14:50:55 +0000

    removed Lambda-specific code from ruthenium.js

---
+++ modules/middlewares/LLoadMetadata.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const mark      = require ( '/var/task/modules/mark' )            
+const DDBDC     = require ( '/var/task/io/DDBDC.js' )
+
+const LLoadMetadata = async ( data ) => {
+    
+    data.RU.metadata = await DDBDC.get ( {
+        TableName:              'TEST-APP-STASH',
+        Key:                    'metadata',
+        ReturnConsumedCapacity: 'TOTAL'
+    } ).promise()
+
+    
+    mark ( `LLoadMetadata.js EXECUTED` )
+    
+    return data
+
+}
+
+module.exports = LLoadMetadata
+mark ( `LLoadMetadata.js LOADED` )
--- tasks/restful/getAllSchemas.js
+++ tasks/restful/getAllSchemas.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const mark      = require ( '/var/task/modules/mark' )            
+const mark      = require ( '/var/task/modules/mark' )
 const DDBDC     = require ( '/var/task/io/DDBDC.js' )
 
 const getAllSchemasTask = async ( data ) => {
