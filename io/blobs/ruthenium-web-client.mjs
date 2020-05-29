7b057f1551ecb0cf122ceb9fbb675d25b915c6d7
jerng-machines <sbx_user1051@169.254.162.133>
2020-05-29 15:48:53 +0000

    documentation chart improved

--- io/blobs/ruthenium-web-client.mjs
+++ io/blobs/ruthenium-web-client.mjs
@@ -1,12 +1,26 @@
 'use strict'
 
-// https://jakearchibald.com/2017/es-modules-in-browsers/
+//  LITERATURE REVIEW:
+//  -   https://jakearchibald.com/2017/es-modules-in-browsers/
+
+
+//  RUNS WHEN (document)'s markup has parsed
+document.addEventListener('DOMContentLoaded', (event) => {
+    
+})
+
+//  RUNS WHEN (document)'s dependencies have all loaded (images, scripts, etc.)
+window.addEventListener('load', (event) => {
+
+    
+});
 
 const removeClosest 
-    = ( thisArg, selector ) =>  thisArg.closest( selector ).remove() 
+    = function (  selector ) {  this.closest( selector ).remove() }
 
+/*
 window.RU = {
 
     removeClosestTr : thisArg => removeClosest ( thisArg, 'tr' )
 
-}
+}*/
