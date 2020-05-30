a0d38490161a3c2829d72efd84c43d560b13b5fb
jerng-machines <sbx_user1051@169.254.252.37>
2020-05-30 19:49:24 +0000

    -

--- modules/middlewares/lambda-normalize-query-string-parameters.js
+++ modules/middlewares/lambda-normalize-query-string-parameters.js
@@ -3,7 +3,9 @@ const lambdaNormalizeQueryStringParameters = async ( data ) => {
     if (data.RU.request.queryStringParameters) {
         for ( const prop in data.RU.request.queryStringParameters ) {
             data.RU.request.queryStringParameters[ prop ] 
-                = data.RU.request.queryStringParameters[ prop ].split(',')
+                =   [ 
+                        data.RU.request.queryStringParameters[ prop ] 
+                    ]
         }
         
     }
