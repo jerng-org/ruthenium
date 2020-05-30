577ca398b23e99bf19b2059679a400c4175155d3
jerng-machines <sbx_user1051@169.254.32.85>
2020-05-30 16:57:48 +0000

    -

--- tasks/restful/forms/desk-schemas-post/markup.js
+++ tasks/restful/forms/desk-schemas-post/markup.js
@@ -10,14 +10,14 @@ const createDeskSchema = async ( data ) => {
 <form method="post" action="/test-middleware?ruthenium=restful&type=desk-schemas">
 <fieldset>
     
-    <label      for="schema[desk-name].1">
+    <label      for="desk-schemas[desk-name].1">
         
         Name for this new Desk Schema
         </label>
     
     <input      type="text"  
-                name="schema[desk-schemas-name]"
-                id="schema[desk-schemas-name].1"
+                name="desk-schemas[name]"
+                id="desk-schemas[name].1"
                 placeholder="new desk schema name">       
     
     <table  data-ru-incrementable-group="column-definition"
