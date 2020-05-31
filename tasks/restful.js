665202705f4edcf343bdc37d83fab0ae753205b3
jerng-machines <sbx_user1051@169.254.211.253>
2020-05-31 20:49:42 +0000

    regex work; resume here

--- tasks/restful.js
+++ tasks/restful.js
@@ -144,8 +144,23 @@ switch ( data.RU.request.http.method ) {
     case ( 'DELETE' ):
         break
     case ( 'POST' ):
-        // ^[^\[A-Z]+|\[[^\[A-Z]+\]|\.[0-9]+
-         
+        /*  Is regex even the best approach?
+
+                Some simple valid-character assumptions:
+                
+                the form xxx[xxx][xxx].yy must be adhered to
+                'x' can be any non-uppercase letter, and non-square-bracket
+                'y' must be Arabic numerals only
+                the number of [xxx] blocks is undetermined, but must be greater than 0
+                xxx segments cannot be zero-length, but may be any other length
+                yy segments cannot be zero-length, but may be any other length
+                newlines are not 
+                Context: JavaScript (no look-behind?)            
+        
+            Validation:     /^[^A-Z\[\]\n\r]+(\[[^A-Z\[\]\n\r]+\])+\.[0-9]+$/
+            
+            Demarcation:    /(?<head>^[^A-Z\[\]\n\r]+)|\[(?<segment>[^A-Z\[\]\n\r]+)\]|\.(?<tail>[0-9]+)$/g
+        */
         let temp = {}
         
         for ( const name in data.RU.request.formStringParameters ) {
