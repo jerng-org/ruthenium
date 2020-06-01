6e1204e86f3418c4d8b07703fa558c120e7ace76
jerng-machines <sbx_user1051@169.254.11.13>
2020-06-01 23:19:52 +0000

    refactored (ready to proceed)

--- modules/middlewares/reindex-form-names.js
+++ modules/middlewares/reindex-form-names.js
@@ -158,6 +158,8 @@ const reindexFormNames = async ( data ) => {
                 storeObject['arbitrary'] = 'value'
             }
             
+            testFunction( temp1[name] )
+            
             //build ( temp1[ name ], temp3, data.RU.request.formStringParameters[ name ] )
             
             //temp1 [ name ] = data.RU.request.formStringParameters[ name ]
