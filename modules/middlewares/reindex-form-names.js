a4278c1caa1eeee6010848f854921379caec7e00
jerng-machines <sbx_user1051@169.254.20.229>
2020-06-01 17:12:17 +0000

    -

--- modules/middlewares/reindex-form-names.js
+++ modules/middlewares/reindex-form-names.js
@@ -52,7 +52,7 @@ const reindexFormNames = async ( data ) => {
                 name.matchAll ( lexerRegex ), 
                 match => {
                     
-                    return JSON.stringify( match )
+                    return JSON.stringify( match.groups )
                     /*for ( const group of match.groups ) {
                         
                     }*/
