0cdb1f32b3215964f1964a123b6967fd0005be3c
jerng-machines <sbx_user1051@169.254.82.181>
2020-06-01 00:31:24 +0000

    -

--- modules/git-commit.js
+++ modules/git-commit.js
@@ -29,7 +29,7 @@ Environmental variable with password
 const mark          = require ( '/var/task/modules/mark' )            
 const childProcess  = require('child_process')
 
-const commitMessage = `intermediate progress on regex for form field indexing`
+const commitMessage = `-`
 
 const gitCommit = () => { try {
 
