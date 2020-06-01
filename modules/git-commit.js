6527afba823a10e3e327adc8e71c0ef64bbbc1bb
jerng-machines <sbx_user1051@169.254.18.153>
2020-06-01 18:39:35 +0000

    unscripted HTML forms, [name] demarcation, ready for progress

--- modules/git-commit.js
+++ modules/git-commit.js
@@ -29,7 +29,7 @@ Environmental variable with password
 const mark          = require ( '/var/task/modules/mark' )            
 const childProcess  = require('child_process')
 
-const commitMessage = `-`
+const commitMessage = `unscripted HTML forms, [name] demarcation, ready for progress`
 
 const gitCommit = () => { try {
 
