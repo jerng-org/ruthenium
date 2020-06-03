1d7c0809081f98c170334e50d571da7e867b6607
jerng-machines <sbx_user1051@169.254.125.181>
2020-06-03 16:55:34 +0000

    -

--- modules/git-commit.js
+++ modules/git-commit.js
@@ -29,7 +29,7 @@ Environmental variable with password
 const mark          = require ( '/var/task/modules/mark' )            
 const childProcess  = require('child_process')
 
-const commitMessage = `prepared documentation for improvement`
+const commitMessage = `-`
 
 const gitCommit = () => { try {
 
