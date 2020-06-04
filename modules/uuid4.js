e59f820e171392a04875f34456111e890ff6218e
jerng-machines <sbx_user1051@169.254.54.173>
2020-06-04 17:35:45 +0000

    -

---
+++ modules/uuid4.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/*      https://stackoverflow.com/posts/2117523/revisions
+
+*/
+
+const mark      = require ( '/var/task/modules/mark' )            
+
+const crypto    = require("crypto")
+
+const uuid4     = () => {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    // (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    (c ^ crypto.randomFillSync(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  )
+}
+
+module.exports  = uuid4
+mark (`uuid4.js LOADED`, true)
