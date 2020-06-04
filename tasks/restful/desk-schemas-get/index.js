379a654a7c0ef387575d850b27c4e9b02193c61c
jerng-machines <sbx_user1051@169.254.184.149>
2020-06-04 20:01:10 +0000

    -

--- tasks/restful/desk-schemas-get/index.js
+++ tasks/restful/desk-schemas-get/index.js
@@ -7,7 +7,7 @@ const markup
     = require ( '/var/task/tasks/restful/desk-schemas-get/markup.js' )
 
 const deskSchemasGet = async ( data ) => {
-    
+
     data.RU.io.deskSchemasScan = await ddbdc.scan ( {
         TableName: 'TEST-APP-DESK-SCHEMAS',
         ReturnConsumedCapacity : 'TOTAL'
--- tasks/restful/desk-schemas-post/index.js
+++ tasks/restful/desk-schemas-post/index.js
@@ -3,9 +3,67 @@
 const ddbdc     
     = require ( '/var/task/io/ddbdc.js' )
 
+const uuid4     
+    = require ( '/var/task/modules/uuid4.js' )
+
 const deskSchemasPost = async ( data ) => {
   
-  throw new Error ( JSON.stringify( data.RU.request.formStringParameters, null, 4 ) ) 
+    //  throw new Error ( JSON.stringify( data.RU.request.formStringParameters, null, 4 ) ) 
+
+    const candidate = data.RU.request.formStringParameters
+
+    //  begin PROTOTYPICAL data validation process:
+    
+    //  VAL_OP#1
+    if ( !  (   'desk-schemas'  in candidate 
+                && 'name'       in candidate[ 'desk-schemas' ]
+                && 'columns'    in candidate[ 'desk-schemas' ]
+                && candidate[ 'desk-schemas' ]['columns'] instanceof Array
+                && candidate[ 'desk-schemas' ]['columns'].length > 1
+            ) 
+    ) {
+        throw Error (   `(~/tasks/restful/desk-schemas-post/) candidate 
+                        insertion to (desk-schemas) did not pass validation.` ) 
+    }
+
+    //  VAL_OP#2
+    for ( const column of candidate[ 'desk-schemas' ]['columns'] ) {
+        
+        if ( !  (      'name'   in column
+                    && 'type'   in column
+                    && /[^A-Z\[\]\s]+/.test( column[ 'name' ] ) // reuse blacklist in (reindex-form-names.js)
+                    && /[^A-Z\[\]\s]+/.test( column[ 'type' ] ) 
+                ) 
+        ) {
+            throw Error (   `(~/tasks/restful/desk-schemas-post/) candidate 
+                            insertion to (desk-schemas.columns) did not pass 
+                            validation.` ) 
+        }
+    }
+
+    //  end PROTOTYPICAL data validation process.
+
+    // Provide ID
+    candidate['data-schemas'].id = uuid4()
+
+    // Call storage layer
+    data.RU.io.put = await ddbdc.put ( {
+        
+        TableName: 'TEST-APP-DESK-SCHEMAS',
+        
+        Item: candidate['data-schemas'],            
+        
+        ConditionExpression : 'attribute_not_exists(id)',
+          //  This checks data already in the DB;
+          //  it seems we do not use this for validating data that has yet to
+          //  be inserted into the DB.
+          
+        ReturnConsumedCapacity : 'TOTAL'
+      
+    } ).promise()
+
+    console.warn (`ddbdc.put returned:`, data.RU.io.put ) 
+
 /*
 
 (new)
