826796c68797cc0ead4e29a37e64716fb677df9b
jerng-machines <sbx_user1051@169.254.142.197>
2020-06-05 18:16:50 +0000

    -

---
+++ modules/r-u-s.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const rutheniumUtilitiesSynchronous = {
+    
+    node: {
+        
+        util: require ( 'util' )    
+    }
+}
+
+module.exports  = rutheniumUtilitiesSynchronous
+//mark (`r-u-s.js (ruthenium utilities, synchronous) LOADED`, true)
