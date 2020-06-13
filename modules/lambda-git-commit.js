7be7472b3d6f7f5790851499e07cb87a3057d78f
jerng-machines <sbx_user1051@169.254.2.49>
2020-06-13 11:57:18 +0000

    (rus.validation) redrafted; ready for testin

--- modules/lambda-git-commit.js
+++ modules/lambda-git-commit.js
@@ -29,7 +29,7 @@ Environmental variable with password
 const mark          = require ( '/var/task/modules/mark.js' )
 const childProcess  = require ( 'child_process' )
 
-const commitMessage = `-`
+const commitMessage = `(rus.validation) redrafted; ready for testin`
 
 const lambdaGitCommit =  () => { try {
 
