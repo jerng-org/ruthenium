d67d13e0ba669d473535cb262696be2b77ebc6ad
jerng-machines <sbx_user1051@169.254.26.197>
2020-06-21 12:33:25 +0000

    break for rest

--- modules/middlewares/compose-response.js
+++ modules/middlewares/compose-response.js
@@ -18,56 +18,6 @@ markupFileNames.forEach ( ( current, index, array ) => {
 )
 
 
-/*  Refer to "0. Notes on LAYOUTs, LAYOUT MARKUPs, and their interaction with 
- *  other tasks/markups" in (layout-set-default.js)
- */
-
-/*  Example of Usage :  data.RU.response.body 
- *                          = wrapStringWithLayout ( data.RU.response.body )
- *
- */
- /*
-const wrapStringWithLayout = async ( _data, _string ) => {
-
-    if ( _data.RU.signals.layoutTaskName )
-    {
-        _data.RU.signals.inferredLayoutMarkupName
-            = _data.RU.signals.layoutTaskName + '-markup'
-            
-        if ( _data.RU.signals.inferredLayoutMarkupName in markups )
-        {
-            await markups[_data.RU.signals.inferredLayoutMarkupName](_data)
-        }
-        else
-        {
-            throw Error (   `(compose-response.js),(wrapStringWithLayout) could not 
-                            find (${ 
-                            _data.RU.signals.inferredLayoutMarkupName 
-                            }.js) in the markups directory. That name was specified at
-                            (data.RU.response.inferredLayoutMarkupName).
-                            
-                            The following may be informative:
-                            
-                            ${ await rus.additionalRequestInformation ( _data )}`)
-        }
-    }
-    else
-    {   
-        throw Error (   `(compose-response.js),(wrapStringWithLayout) was called; 
-                        (data.RU.signals.layoutTaskName) was falsy - this is
-                        normally set to a default value in (layout-set-default.js),
-                        but the value may have been unset/erased between there 
-                        and this line; if you wish for (wrapStringWithLayout)
-                        to return the given string as-is, then consider creating
-                        a layout which adds nothing to the given string, and 
-                        then explicitly set that layout's name in 
-                        (data.RU.signals.layoutTaskName). Otherwise, simply do
-                        not run this function;`)
-    }
-
-}
-*/
-
 const composeResponse = async ( data ) => {
     
     if ( data.RU.response ) {
