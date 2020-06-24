81554ce1ebf2fa863c98973d37eb981a30667a32
jerng-machines <sbx_user1051@169.254.26.5>
2020-06-24 08:44:13 +0000

    -

---
+++ configuration.js
@@ -0,0 +1,19 @@
+module.exports = {
+    
+    /*  verbosity
+     *
+     *      0   -   ONLY console.error
+     *      
+     *      1   -   ONLY console.error, console.warn
+     *
+     *      2   -   ONLY console.error, console.warn, console.info
+     *
+     *      3   -   (all console calls), some HTML output
+     *
+     *      4   -   undefined so far
+     *
+     *
+     */
+    
+    verbosity:  3
+}
