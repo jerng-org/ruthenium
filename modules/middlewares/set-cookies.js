2877a7f86ffaea7090dbdae5a054f1e1eed825e8
jerng-machines <sbx_user1051@169.254.2.25>
2020-06-26 09:22:53 +0000

    -

--- modules/middlewares/set-cookies.js
+++ modules/middlewares/set-cookies.js
@@ -5,12 +5,12 @@ const rus = require ( '/var/task/modules/r-u-s.js' )
 //  Provide a debuggable function name, 
 //  in order to avoid debugging (function).toString()
 
-const thisIsMyName = async ( data ) => {
+const setCookies = async ( data ) => {
     
     // rutheniumReducer.js will mark() execution, you don't have to
     
     return data
 }
 
-module.exports = thisIsMyName
-rus.mark (`~/modules/middlewares/this-is-my-name.js LOADED`)
+module.exports = setCookies
+rus.mark (`~/modules/middlewares/set-cookies.js LOADED`)
