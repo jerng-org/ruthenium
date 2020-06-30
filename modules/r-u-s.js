0a0bc9fc2a689c429d4130313bc49b29a8d912f9
jerng-machines <sbx_user1051@169.254.166.253>
2020-06-30 22:51:20 +0000

    -

--- modules/r-u-s.js
+++ modules/r-u-s.js
@@ -187,7 +187,7 @@ DEBUG EVERYTHING:
             async object => rus.node.util.inspect( object, { depth: Infinity } ),
         
         stringify4:
-            async object => ( '' + JSON.stringify( object, null, 4 ) ).replace(/\\n/g, '\n'),
+            async object => ( "(rus.print.stringify4):\n" + JSON.stringify( object, null, 4 ) ).replace(/\\n/g, '\n'),
 
         xml300:
             async xmlString => 
