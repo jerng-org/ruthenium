90c96a68d48ff99b0779d02fe5c1a82b579ce5c1
jerng-machines <sbx_user1051@169.254.35.125>
2020-07-01 15:54:09 +0000

    -

--- modules/cookie.js
+++ modules/cookie.js
@@ -5,6 +5,19 @@ const conf      = require ( `/var/task/configuration.js` )
 
 /*  QUICK START
  *
+ *      PREFER:     rus.cookie.__HostSet   ( suffix, value )
+ *                  rus.cookie.__HostExpire( suffix )
+ *
+ *                  ... over:
+ *
+ *                  rus.cookie.__SecureSet   ( suffix, value )
+ *                  rus.cookie.__SecureExpire( suffix )
+ *
+ *                  ... over:
+ *
+ *                  rus.cookie.set   ( name, value )
+ *                  rus.cookie.expire( name )
+ *
  *  -   call (rus.cookie) .set( stringName, value) and .expire( stringName ) );
  *
  *      -   you can refine (stringName) to << explaination of (idObject) >>
@@ -351,3 +364,55 @@ CASTLE.TLD, unless you really need the user to eyeball the asset request. Hmm
 A version of this is mentionedin RFC 6265bis.8.8.2.
 
 */
+
+/*  TEST NOTES:
+ *  
+ *  'use strict'
+ *  
+ *  const rus = require ( '/var/task/modules/r-u-s.js' )
+ *  
+ *  /// This middleware looks in (data.RU.signals.sendResponse.setCookies)
+ *  //
+ *  /// Framework users may call (rus.cookie.<<various>>) to manipulate the above.
+ *  ///
+ *  
+ *  
+ *  const cookieTest = async ( data ) => {
+ *  
+ *      //rus.cookie.__HostSet ( data, 'cookie_name', 'cookie_value' )
+ *      //rus.cookie.__SecureSet ( data, 'cookie_name', 'cookie_value' )
+ *      //rus.cookie.set ( data, 'cookie_name', 'cookie_value', {
+ *      //    Path:   false,
+ *      //    //Path: '/test-middleware',
+ *      //    Domain: false,
+ *      //    //Domain: 'secure.api.sudo.coffee',
+ *      //    Secure: false,
+ *      //    HttpOnly:   false,
+ *      //    Expires:    false,
+ *      //    ['Max-Age']:false,
+ *      //    SameSite:   false,
+ *      //    //SameSite: 'None', // no problem
+ *      //    //SameSite: 'Lax' // no problem
+ *      //    SameSite: 'Strict' // bad
+ *      //} )
+ *      //rus.cookie.expire ( data, 'cookie_name' )
+ *      //rus.cookie.__SecureExpire ( data, 'cookie_name' )
+ *      //rus.cookie.__HostExpire ( data, 'cookie_name' )
+ *      
+ *      const body = `
+ *          <h1>cookie debugging</h1>
+ *          <pre>${
+ *          await rus.print.stringify4 ( data.RU.request.headers.cookies )
+ *          } </pre>`
+ *  
+ *      data.RU.signals.sendResponse
+ *          = { ... data.RU.signals.sendResponse, body: body } 
+ *  
+ *      rus.mark ( `cookie-test.js EXECUTED` )
+ *  }
+ *  // manipulate (data.RU), for example
+ *  // no need to return (data)
+ *  
+ *  module.exports = cookieTest
+ *  rus.mark ( `~/tasks/cookie-test.js LOADED` )
+ */
