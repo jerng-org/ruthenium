a965aaa09fbe414ea3fbba1d20d6cc68bfc5be4e
jerng-machines <sbx_user1051@169.254.19.117>
2020-07-01 00:15:51 +0000

    (cookie-test.js) still stuck with SameSite=Strict

--- tasks/cookie-test.js
+++ tasks/cookie-test.js
@@ -10,7 +10,7 @@ const rus = require ( '/var/task/modules/r-u-s.js' )
 
 const cookieTest = async ( data ) => {
 
-    /*
+    //*
     rus.cookie.set ( data, 'cookie_name', 'cookie_value', {
         Path:   false,
         //Path: '/test-middleware',
@@ -26,7 +26,7 @@ const cookieTest = async ( data ) => {
         SameSite: 'Strict' // bad
     } )
     //*/
-    rus.cookie.expire ( data, 'cookie_name' )
+    //rus.cookie.expire ( data, 'cookie_name' )
     
     const body = `
         <h1>cookie debugging</h1>
