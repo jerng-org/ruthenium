5597a27a758c00014ddbedf50fb1c5629d60c185
jerng-machines <sbx_user1051@169.254.209.149>
2020-07-02 17:58:10 +0000

    -

---
+++ modules/cognito.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const mark      = require ( '/var/task/modules/mark.js' )
+
+const thisIsMyName     = async () => {
+  
+}
+
+module.exports  = thisIsMyName
+mark (`~/modules/thisIsMyName.js LOADED`)
