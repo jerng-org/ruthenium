4943c50b2fe28379a30b65763a7ae9684078f229
jerng-machines <sbx_user1051@169.254.233.197>
2020-07-02 18:00:33 +0000

    (some bugfixes)

--- configuration.js
+++ configuration.js
@@ -40,7 +40,7 @@ module.exports = {
      *
      */
     gitCommit: 1,
-    gitCommitMessage: '-',
+    gitCommitMessage: '(some bugfixes)',
     /*  USES:
      *  Merge order     :   1
      *  Name            :   git-lambda2
