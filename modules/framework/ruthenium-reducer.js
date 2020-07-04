3ca4fcd48dcbb8a8d510c4a9d5ca2115c38ea6d0
jerng-machines <sbx_user1051@169.254.174.37>
2020-07-04 01:33:28 +0000

    -

--- modules/framework/ruthenium-reducer.js
+++ modules/framework/ruthenium-reducer.js
@@ -1,49 +1,46 @@
 'use strict'
 
-const rus 
-    = require ( '/var/task/modules/r-u-s.js' )
+const rus = require('/var/task/modules/r-u-s.js')
 
-const rutheniumReducer = async (    DATA_IN_PROMISE, 
-                                    CURRENT_MIDDLEWARE, 
-                                    INDEX,
-                                    MIDDLEWARE_QUEUE ) => { 
+const rutheniumReducer = async(DATA_IN_PROMISE,
+    CURRENT_MIDDLEWARE,
+    INDEX,
+    MIDDLEWARE_QUEUE) => {
 
-        const DATA                  = await DATA_IN_PROMISE
-        
-        try { 
+    const DATA = await DATA_IN_PROMISE
 
-            const intermediateData  = await CURRENT_MIDDLEWARE ( DATA )
-           
+    try {
 
-            /* THROWN? This line does not run, if CURRENT_MIDDLEWARE erred */
-            
-            rus.mark ( `middleware executed: ${ CURRENT_MIDDLEWARE.name }` )
+        const intermediateData = await CURRENT_MIDDLEWARE(DATA)
 
-            // Validation: as middlewares may return nonsense
-            if (        typeof intermediateData == 'object'
-                    //&&  intermediateData.LAMBDA // (uncomment this when it has been generalised to HOST_LABEL)
-                    &&  intermediateData.RU                    ) 
-            {
-                // ordinary;
-                return  intermediateData
-            }
-            else 
-            if (        INDEX + 1 == MIDDLEWARE_QUEUE.length )
-            { 
-                // don't complain about the last middleware in queue;
-                // TODO: need to analyse this design decision a bit more;
-                //  Do we really want to admit non-object responses?
-                return  intermediateData 
-            }
-            else        
-            {           
-                // complain loudly
-///////////////////////////////////////////////////////////////////////////////
 
-console.warn ( intermediateData )
-const thingType = typeof intermediateData
+        /* THROWN? This line does not run, if CURRENT_MIDDLEWARE erred */
 
-throw Error (   `ruthenium.js : 
+        rus.mark(`middleware executed: ${ CURRENT_MIDDLEWARE.name }`)
+
+        // Validation: as middlewares may return nonsense
+        if (typeof intermediateData == 'object'
+            //&&  intermediateData.LAMBDA // (uncomment this when it has been generalised to HOST_LABEL)
+            &&
+            intermediateData.RU) {
+            // ordinary;
+            return intermediateData
+        }
+        else
+        if (INDEX + 1 == MIDDLEWARE_QUEUE.length) {
+            // don't complain about the last middleware in queue;
+            // TODO: need to analyse this design decision a bit more;
+            //  Do we really want to admit non-object responses?
+            return intermediateData
+        }
+        else {
+            // complain loudly
+            ///////////////////////////////////////////////////////////////////////////////
+
+            console.warn(intermediateData)
+            const thingType = typeof intermediateData
+
+            throw Error(`ruthenium.js : 
                 
     a middleware, (${ ( CURRENT_MIDDLEWARE.name
                         ?   CURRENT_MIDDLEWARE.name
@@ -71,43 +68,52 @@ ${
     ${ rus.stringify ( DATA ) }
 `)
 
-/*  More sensitive, related, information:
+            /*  More sensitive, related, information:
 
-    (1)        
-        util.inspect ( CURRENT_MIDDLEWARE, { 
-                depth:      Infinity, 
-                showHidden: true
-        } )
+                (1)        
+                    util.inspect ( CURRENT_MIDDLEWARE, { 
+                            depth:      Infinity, 
+                            showHidden: true
+                    } )
+                    
+                (2)
+                    CURRENT_MIDDLEWARE.toString().slice( 0, 100 )
+            */
+
+            ///////////////////////////////////////////////////////////////////////////////
+
+        }
+        // end ELSE
+
+    }
+    catch (e) {
         
-    (2)
-        CURRENT_MIDDLEWARE.toString().slice( 0, 100 )
-*/
+        //  1.
+        //  Caches the error
+        DATA.RU.errors.push({
+            'typeof': typeof e,
+            thrown: e,
+        })
+        console.error(rus.conf.labels.reducer500Body, "\n", e, "\n", DATA)
 
-///////////////////////////////////////////////////////////////////////////////
-            
-            }
-            // end ELSE
-
-        } 
-        catch (e) 
+        //  2.
+        if (INDEX + 1 == MIDDLEWARE_QUEUE.length)
         {
-            DATA.RU.errors.push ( {
-                'typeof':   typeof e,
-                thrown:     e,
-            } )
-            console.error ( rus.conf.labels.reducer500Body, "\n", e, "\n", DATA )
-
-            if ( INDEX + 1 == MIDDLEWARE_QUEUE.length )
-            {
-                // last middleware is special
-                return  {   statusCode: 500,
-                            body:       rus.conf.labels.reducer500Body
-                        }
-            }
-            else 
-            {
-                return DATA
-            }
+            //  2.a.
+            //  the last middleware is special;
+            return {    statusCode: 500,
+                        body: rus.conf.labels.reducer500Body    }
+        }
+        else
+        {
+            //  2.b.
+            //  returning DATA here continues to attempt to execute subsequent
+            //  middlewares; this is NOT preferable if we need a hard exit
+            //  from the pipeline; but this is preferable if we have a 
+            //  soft process that tolerates failure; so there should be a switch
+            //  here
+            return DATA
         }
     }
+}
 module.exports = rutheniumReducer
