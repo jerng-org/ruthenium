b8745a7d8095b0c2793db943552b3cf799b82e86
jerng-machines <sbx_user1051@169.254.217.45>
2020-07-04 02:19:14 +0000

    -

--- configuration.js
+++ configuration.js
@@ -18,7 +18,7 @@ module.exports = {
      *
      */
     gitCommit: 1,
-    gitCommitMessage: '(oidc-validation.js) middleware prepared;',
+    gitCommitMessage: '-',
     /*  USES:
      *  Merge order     :   1
      *  Name            :   git-lambda2
