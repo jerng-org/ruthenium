3a6bfcfa37b3f5adf45a798830493bff0719af8f
jerng-machines <sbx_user1051@169.254.21.45>
2020-07-04 21:43:33 +0000

    -

--- modules/middlewares/oidc-validation.js
+++ modules/middlewares/oidc-validation.js
@@ -1,18 +1,21 @@
 'use strict'
 
-const rus = require ( '/var/task/modules/r-u-s.js' )
+const rus = require('/var/task/modules/r-u-s.js')
 
 //  Provide a debuggable function name, 
 //  in order to avoid debugging (function).toString()
 
-const thisIsMyName = async ( data ) => {
-    
-    // rutheniumReducer.js will mark() execution, you don't have to
-    
-    throw Error (`test error in oidc-validation.js`)
-    
-    return data
+const thisIsMyName = async(data) => {
+
+    let validationFailure
+
+    if (validationFailure) {
+        throw Error(`test error in oidc-validation.js`)
+    }
+    else {
+        return data
+    }
 }
 
 module.exports = thisIsMyName
-rus.mark (`~/modules/middlewares/this-is-my-name.js LOADED`)
+rus.mark(`~/modules/middlewares/this-is-my-name.js LOADED`)
