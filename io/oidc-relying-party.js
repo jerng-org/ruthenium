558bde811e54346f4e870ece0f4c7f3719a19a84
jerng-machines <sbx_user1051@169.254.223.141>
2020-07-05 23:45:39 +0000

    -

--- io/oidc-relying-party.js
+++ io/oidc-relying-party.js
@@ -204,7 +204,7 @@ console.log(`(oidc-relying-party.js) issuerExchangeRequestBody (before encoding)
                     if (splitJwt.length != 3) { throw Error(`
                     (oidc-relying-party.js) 7.1.2. (processToken) tried to split
                     (token), expected to find three (3) sections, but did not 
-                    find (3) sections; the Sections found: ${splitJwt}`) }
+                    find (3) sections; the Sections found: ${ rus.print.stringify4(splitJwt) }`) }
 
                     let decodedSections = splitJwt.map(s => {
                         let decodedSection = Buffer.from(s, 'base64').toString('utf8')
