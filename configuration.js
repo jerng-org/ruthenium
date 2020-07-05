8f9a1c00465bea655953e9778451372d249d3e29
jerng-machines <sbx_user1051@169.254.32.205>
2020-07-05 07:06:13 +0000

    WIP plumbing: (oidc-validation) and (oidc-relying-party)

--- configuration.js
+++ configuration.js
@@ -18,7 +18,7 @@ module.exports = {
      *
      */
     gitCommit: 1,
-    gitCommitMessage: '(-)',
+    gitCommitMessage: 'WIP plumbing: (oidc-validation) and (oidc-relying-party)',
     /*  USES:
      *  Merge order     :   1
      *  Name            :   git-lambda2
