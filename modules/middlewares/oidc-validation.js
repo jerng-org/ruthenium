9a17d1a086f45b062e1dee6b8fee246211a69152
jerng-machines <sbx_user1051@169.254.32.53>
2020-07-06 20:59:41 +0000

    cognito OIDC authorization code flow established (not the cleanest code); next, to add sessions; guest routes;

--- modules/middlewares/oidc-validation.js
+++ modules/middlewares/oidc-validation.js
@@ -4,14 +4,13 @@ const rus = require('/var/task/modules/r-u-s.js')
 
 /*  Roughly, this middleware tries to ...
  *
- *  validate                (data.RU.queryStringParameters.code) ...
+ *  validate                   (data.RU.queryStringParameters.code) ...
  *  
- *  ... by calling          (rus.aws.cognito)
+ *      ... by calling          (rus.aws.cognito)
  *
- *      ... which calls     (~/io/cognito.js)
+ *          ... which calls     (~/io/cognito-oidc-relying-party.js)
  *
- *          ... which calls (~/io/oidc-relying-party.js);
- *
+ *  THEN, it stores the keys (id_token) and (access_token) in (data.RU.signals.)
  *
  *
  */
@@ -22,7 +21,7 @@ const oidcValidation = async(data) => {
         data.RU.signals.oidc = await rus.aws.cognito.authorizationCodeFlowJwtValidation(data.LAMBDA.event.queryStringParameters.code)
     }
     catch (e) {
-        console.warn(`Middleware (oidc-validation.js) failed, (error):`, e, `(data):`, data)
+        console.warn(`Middleware (oidc-validation.js) failed, (error):`, e)
     }
     return data
 }
