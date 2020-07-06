b14ea4dbf86bc353a09f70352197e62b203b777d
jerng-machines <sbx_user1051@169.254.55.85>
2020-07-06 03:45:50 +0000

    -

--- io/cognito-oidc-relying-party.js
+++ io/cognito-oidc-relying-party.js
@@ -2,10 +2,15 @@
 
 const mark = require('/var/task/modules/mark.js')
 
+/*  The Authorization Server a.k.a. Issuer (Cognito) chooses within its rights
+ *  granted by the OAuth 2.0 (RFC 6749.4.1.2.) to never accept the same (code)
+ *  twice;
+ */
+
 const authorizationCodeFlowJwtValidation = async code => {
 
     //  EXIT_OPPORTUNITY_1
-    if (!code) throw Error(`(oidc-relying-party.js) 0. : (code) was falsy`)
+    if (!code) throw Error(`(cognito-oidc-relying-party.js) 0. : (code) was falsy`)
 
     //  1.1.  
     //  Node modules
@@ -145,7 +150,7 @@ const authorizationCodeFlowJwtValidation = async code => {
     //  
     //  })
 
-    console.log(`(oidc-relying-party.js) BEFORE PROMISE.ALL`)
+    console.log(`(cognito-oidc-relying-party.js) BEFORE PROMISE.ALL`)
 
     //  7.
     //  (.all) resolves only if (all its children) resolve; if any child rejects
@@ -162,7 +167,7 @@ const authorizationCodeFlowJwtValidation = async code => {
 
                 const [issuerExchangeResponseBody, issuerJwksResponseBody] = resolvedValues
 
-                console.log(`(oidc-relying-party.js) PROMISE.ALL.THEN`,
+                console.log(`(cognito-oidc-relying-party.js) PROMISE.ALL.THEN`,
                     `IERB:`,
                     `<<${typeof issuerExchangeResponseBody}>>`,
                     issuerExchangeResponseBody,
@@ -172,7 +177,7 @@ const authorizationCodeFlowJwtValidation = async code => {
                 )
 
                 //  EXIT_OPPORTUNITY_2
-                if (!issuerExchangeResponseBody) throw Error(`(oidc-relying-party.js) 
+                if (!issuerExchangeResponseBody) throw Error(`(cognito-oidc-relying-party.js) 
                 7. : (issuerExchangeResponseBody) was falsy : we sent a HTTP request 
                 containing (code) to the OIDC issuer, its HTTP response body
                 was falsy;`)
@@ -184,101 +189,118 @@ const authorizationCodeFlowJwtValidation = async code => {
                 //  Extract unprocessed tokens from 4.3.
                 const parsedIssuerExchangeResponseBody = JSON.parse(issuerExchangeResponseBody)
 
-                if (!parsedIssuerExchangeResponseBody) {
+                /*  What is expected here (specific to AWS Cognito, for now, 2020-07-06)
+                 *  {   
+                 *      id_token:       << string, delimited by '.', three segments >>>
+                 *                      << RFC 7515, JSON Web Signature, Compact Serialization https://tools.ietf.org/html/rfc7515#section-3.1 >>
+                 * 
+                 *                      BASE64URL(UTF8(JWS Protected Header)) || '.' ||
+                 *                      BASE64URL(JWS Payload) || '.' ||
+                 *                      BASE64URL(JWS Signature)
+                 *
+                 *      access_token:  << as above (see id_token) >> 
+                 *
+                 *      refresh_token:  << string, delimited by '.', FIVE segments >>>
+                 *                      << RFC 7516.3.1, JSON Web Encryption Compact Serialization https://tools.ietf.org/html/rfc7516#section-3.1 >>
+                 *
+                 *                      BASE64URL(UTF8(JWE Protected Header)) || '.' ||
+                 *                      BASE64URL(JWE Encrypted Key) || '.' ||
+                 *                      BASE64URL(JWE Initialization Vector) || '.' ||
+                 *                      BASE64URL(JWE Ciphertext) || '.' ||
+                 *                      BASE64URL(JWE Authentication Tag)
+                 *
+                 *      expires_in:     << SECONDS, integer >>
+                 *
+                 *      token_type:     "Bearer"
+                 *
+                 */
+
+                /*  TODO use the Ruthenium JSON validator here later: */
+
+                if (typeof parsedIssuerExchangeResponseBody != 'object') {
                     // EXIT_OPPORTUNITY_3
-                    throw Error(`(oidc-relying-party.js) 7.1.2.
-                    (parsedIssuerExchangeResponseBody) was found to be falsy;`)
+                    throw Error(`(cognito-oidc-relying-party.js) 7.1.2.
+                    (typeof parsedIssuerExchangeResponseBody) was (not 'object');`)
                 }
                 else
                 if ('error' in parsedIssuerExchangeResponseBody) {
                     // EXIT_OPPORTUNITY_4
-                    throw Error(`(oidc-relying-party.js) 7. 
-                    (parsedIssuerExchangeResponseBody.error) was found to be 
-                    "${ parsedIssuerExchangeResponseBody.error }";`)
+                    throw Error(`(cognito-oidc-relying-party.js) 7. 
+                    (parsedIssuerExchangeResponseBody.error) was 
+                    ("${ parsedIssuerExchangeResponseBody.error }");`)
 
                     // Expected error values: https://tools.ietf.org/html/rfc6749#section-5.2
                 }
-                
-                console.log(`(oidc-relying-party.js)`,
-                    `parsedIssuerExchangeResponseBody.id_token:
-                    
-                    `,
-                    parsedIssuerExchangeResponseBody.id_token.split('.'),
-                    `parsedIssuerExchangeResponseBody.access_token:
-                    
-                    `,
-                    parsedIssuerExchangeResponseBody.access_token.split('.'),
-                    `parsedIssuerExchangeResponseBody.refresh_token:
-                    
-                    `,
-                    parsedIssuerExchangeResponseBody.refresh_token.split('.'),
-                )
-                
-                console.log(`(oidc-relying-party.js)`,
-                    `parsedIssuerExchangeResponseBody.id_token:
-                    
-                    `,
-                    parsedIssuerExchangeResponseBody.id_token.split('.').map(s => {
-                        let decodedSection = Buffer.from(s, 'base64').toString('utf8')
-                        return [typeof decodedSection, decodedSection ]
-                    }),
-                    `parsedIssuerExchangeResponseBody.access_token:
-                    
-                    `,
-                    parsedIssuerExchangeResponseBody.access_token.split('.').map(s => {
-                        let decodedSection = Buffer.from(s, 'base64').toString('utf8')
-                        return [typeof decodedSection, decodedSection ]
-                    }),
-                    `parsedIssuerExchangeResponseBody.refresh_token:
-                    
-                    `,
-                    parsedIssuerExchangeResponseBody.refresh_token.split('.').map(s => {
-                        let decodedSection = Buffer.from(s, 'base64').toString('utf8')
-                        return [typeof decodedSection, decodedSection ]
-                    }),
-                )
-                
-throw Error ('artificial stop')
+                else
+                if ('id_token' in parsedIssuerExchangeResponseBody &&
+                    'access_token' in parsedIssuerExchangeResponseBody &&
+                    'refresh_token' in parsedIssuerExchangeResponseBody &&
+                    'expires_in' in parsedIssuerExchangeResponseBody &&
+                    'token_type' in parsedIssuerExchangeResponseBody) {
+                    // EXIT_OPPORTUNITY_5
+                    throw Error(`(cognito-oidc-relying-party.js) 7. 
+                    (parsedIssuerExchangeResponseBody) did not have all expected
+                    keys; found keys:
+                    ("${ Object.keys( parsedIssuerExchangeResponseBody ) }");`)
 
-                // If checks pass, then default:
-                const tokens = parsedIssuerExchangeResponseBody
+
+                }
 
                 //  7.1.2.
                 //  Define operations to process (7.1.)
-                const processToken = token => {
+                const processToken = compactSerialization => {
 
-                    let splitJwt = token.split('.')
-
-                    // EXIT_OPPORTUNITY_5
-                    if (splitJwt.length != 3) { throw Error(`
-                    (oidc-relying-party.js) 7.1.2. (processToken) tried to split
-                    (token), expected to find three (3) sections, but did not 
-                    find (3) sections; the Sections found: ${ JSON.stringify(splitJwt,null,4) }`) }
-
-                    let decodedSections = splitJwt.map(s => {
-                        let decodedSection = Buffer.from(s, 'base64').toString('utf8')
-                        return decodedSection
+                    let decodedSections = compactSerialization.split('.').map(s => {
+                        return Buffer.from(s, 'base64').toString('utf8')
                     })
-
-                    let parsedSections = {
-                        header: JSON.parse(decodedSections[0]),
-                        payload: JSON.parse(decodedSections[1]),
-                        signature: decodedSections[2]
-                    }
-
-                    return parsedSections
+                    /*
+                                        let parsedSections = {
+                                            header: JSON.parse(decodedSections[0]),
+                                            payload: JSON.parse(decodedSections[1]),
+                                            signature: decodedSections[2]
+                                        }
+                                        return parsedSections
+                    */
+                    return decodedSections
                 }
-
+                
                 // 7.1.3.
                 // Apply (7.1.2.) to (7.1.1.)
 
+                //  What is expected here (specific to AWS Cognito, for now, 2020-07-06)
+
+                const processedTokens = {
+                    id_token: processToken(parsedIssuerExchangeResponseBody.id_token),
+                    access_token: processToken(parsedIssuerExchangeResponseBody.access_token),
+                    refresh_token: processToken(parsedIssuerExchangeResponseBody.refresh_token)
+                }
+
+                //*                
+                console.log(`(cognito-oidc-relying-party.js)
+                    
+                    processedTokens:
+                                    
+                                    `,
+                    processedTokens
+                )
+                throw Error('artificial stop')
+                //*/
+
+
+                /*
+                                    // EXIT_OPPORTUNITY_5
+                                    if (splitJwt.length != 3) { throw Error(`
+                                    (cognito-oidc-relying-party.js) 7.1.2. (processToken) tried to split
+                                    (token), expected to find three (3) sections, but did not 
+                                    find (3) sections; the Sections found: ${ JSON.stringify(splitJwt,null,4) }`) }
+                */
+                /*
                 let processedTokens = {}
                 for (const key in tokens) {
                     processedTokens[key] = processToken(tokens[key])
                 }
-                console.log(`(io/oidc-relying-party.js) 7.1.3.: (processedTokens):`, processedTokens)
+                console.log(`(io/cognito-oidc-relying-party.js) 7.1.3.: (processedTokens):`, processedTokens)
 
-                /*
                 const processedTokens = {
                     id_token: processToken(tokens.id_token),
                     access_token: processToken(tokens.access_token),
@@ -350,7 +372,7 @@ throw Error ('artificial stop')
 
                 //  7.4.2.
                 //  Attempt validation;
-                console.log(`(io/oidc-relying-party.js) 7.4.2.: before conditionals`,
+                console.log(`(io/cognito-oidc-relying-party.js) 7.4.2.: before conditionals`,
                     `
 
 tokenValidationArguments.id_token:`,
@@ -365,7 +387,7 @@ tokenValidationArguments.access_token:`,
                 if (tokenValidationArguments.id_token &&
                     tokenValidationArguments.id_token.pem &&
                     tokenValidationArguments.id_token.alg) {
-                    console.log(`(io/oidc-relying-party.js) 7.4.2.: before try, to validate (id_token)`)
+                    console.log(`(io/cognito-oidc-relying-party.js) 7.4.2.: before try, to validate (id_token)`)
                     try {
                         tokenValidatedPayloads.id_token = jsonwebtoken.verify(
                             tokenValidationArguments.id_token.token,
@@ -380,7 +402,7 @@ tokenValidationArguments.access_token:`,
                 if (tokenValidationArguments.access_token &&
                     tokenValidationArguments.access_token.pem &&
                     tokenValidationArguments.access_token.alg) {
-                    console.log(`(io/oidc-relying-party.js) 7.4.2.: before try, to validate (access_token)`)
+                    console.log(`(io/cognito-oidc-relying-party.js) 7.4.2.: before try, to validate (access_token)`)
                     try {
                         tokenValidatedPayloads.access_token = jsonwebtoken.verify(
                             tokenValidationArguments.access_token.token,
@@ -393,7 +415,7 @@ tokenValidationArguments.access_token:`,
                     }
                 }
 
-                console.log(`(io/oidc-relying-party.js) 7.4.2.:`, tokenValidatedPayloads)
+                console.log(`(io/cognito-oidc-relying-party.js) 7.4.2.:`, tokenValidatedPayloads)
 
                 //  THE FOLLOWING SECTIONS ARE MORE USEFUL WHEN THIS SCRIPT IS BEING 
                 //  TESTED IN A STANDALONE CONTEXT; HERE IT IS WRAPPED IN A WEB 
@@ -468,21 +490,166 @@ tokenValidationArguments.access_token:`,
 
             rejectedReason => {
                 //callback(rReason)
-                console.error(`(~/io/oidc-relying-party.js) algorithm section 
+                console.error(`(~/io/cognito-oidc-relying-party.js) algorithm section 
                                 7.x; Promise.all was rejected with 
                                 reason:`, rejectedReason)
                 return 'placeholder-return-value-for:authorizationCodeFlowJwtValidation: Promise.all REJECTED'
             }
         )
     // end section (7.x) 
-    console.log(`(oidc-relying-party.js) AFTER PROMISE.ALL`)
+    console.log(`(cognito-oidc-relying-party.js) AFTER PROMISE.ALL`)
 
     return 'placeholder-return-value-for:authorizationCodeFlowJwtValidation DEFAULT'
 }
 
-const oidcRelyingParty = {
+const cognitoOidcRelyingParty = {
     authorizationCodeFlowJwtValidation: authorizationCodeFlowJwtValidation
 }
 
-module.exports = oidcRelyingParty
-mark(`~/io/oidcRelyingParty.js LOADED`)
+module.exports = cognitoOidcRelyingParty
+mark(`~/io/cognitoOidcRelyingParty.js LOADED`)
+
+/*  
+
+2020-07-03 Notes on Cognito integration go here, temporarily:  
+
+Items below describe the Authorisation Server / Issuer implementation; for the 
+Relying Party logic, refer to (cognito-oidc-relying-party.js).
+
+Things which appear to be missing from the Cognito user experience:
+
+    -   Entity Relationship Diagram for its entire OIDC Issuer implementation,
+        hosted UI, and platform specific ontologies;
+        
+    -   OIDC Relying Party client libraries customised to match its Issuer 
+        implementation;
+
+1. Setting up AWS Cognito: at a glance
+   
+    Cognito > has many User Pools > each User Pool has many Apps
+        
+   -Cognito
+    |
+    +-User Pools :  "for authentication, including federated
+    |               identification via third-party IdP (Identity
+    |               Provider)"
+    |
+    +-Identity Pools :  "for authorization to AWS resources"
+      |
+      +-<< STRING0 >> : "a user pool name" (one of many)
+        |
+        |     //////////
+        |     //      //
+        |     //  !!  //  Make way.
+        |     //      //
+        |     //////////
+        | 
+        +-General Settings
+        | |
+        | +-<< MANY ITEMS UNDOCUMENTED >>
+        |
+        |     //////////
+        |     //      //
+        |     //  !!  //  Make way.
+        |     //      //
+        |     //////////
+        | 
+        +-Federation
+        | |
+        | +-<< MANY ITEMS UNDOCUMENTED >>
+        |
+        |     //////////
+        |     //      //
+        |     //  !!  //  Make way.
+        |     //      //
+        |     //////////
+        | 
+        +-App Integration
+          |
+          +-UI Customisation
+          |
+          +-Resource Servers
+          |
+          +-App Client Settings
+          | |
+          | +-<< STRING9 >> : "an app name" (each user pool has many apps)
+          |   |
+          |   +-ID : << STRING6 >>
+          |   |
+          |   +-Enabled Identity Providers : << CHECKBOXES >>
+          |   |
+          |   +-URLs
+          |   | |
+          |   | +-Callback URL(s) : << STRING7 >> "comma separated"
+          |   | |
+          |   | +-Sign Out URL(s) : << STRING8 >> "comma separated"
+          |   |
+          |   +-OAuth2.0
+          |     |
+          |     +-Allowed OAuth Flows : << CHECKBOXES >>
+          |     |
+          |     +-Allowed OAuth Scopes : << CHECKBOXES >>
+          |     |
+          |     +-Hosted UI : << STRING1 >>, and << STRING2 >>
+          |
+          |
+          +-Domain Name
+            |
+            +-Amazon Cognito Domain : << STRING1 >> "https://<< STRING1.1 >>.auth.<< AWS_REGION >>.amazoncognito.com"
+            |
+            +-Your Own Domain : "Must have associated certificate in ACM (AWS Certificate Manager)"
+              |
+              +-Domain Status : "ACTIVE" ?
+              |
+              +-Domain Name : << STRING2 >>
+              |
+              +-ACM Certificate : << STRING3 >>
+              |
+              +-Alias Target : << STRING4 >> "must be added to << STRING2 >>'s DNS record";
+                               this seems to take the form of a << STRING5 >>.cloudfront.net
+         
+
+2.  Establish first that <<test user's state>> is <<logged out>>.
+
+3.  View the Login form at << Hosted UI >> at << STRING1 >> or << STRING2 >>
+
+    GET https://<< STRING2 >>/login ?client_id=             << STRING6 >>
+                                    &response_type= code    << limited by Allowed OAuth Flows >>
+                                    &scope=                 << limited by Allowed OAuth Scopes >>
+                                    &redirect_uri=          << STRING7 >> (URL Encoded)
+
+    3a. Optionally launch to (3.) from a prepared link in a webpage at URL 
+        << STRING11 >>; this can be the same as << STRING7 >> for example;
+
+4.  Submit the Login form including fields  << username >>
+                                            << password >>
+                                            << _csrf >>>
+                                            << cognitoAsfData >> (???)
+                                            << signInSubmitButton >>
+
+        via POST << to the URL in (3.) above >>>
+    
+    Server will respond with:
+    
+        302 which redirects to << STRING7 >>    &code=  << STRING10 >>
+
+5.  Back at << STRING7 >>, a script has been monitoring (document.readyState and 
+    the  DOMContentLoaded event), and upon the specified conditions, checks
+    (window.location.search) for the string "code", and IF it finds it:
+
+        THEN, the script sets (document.location) i.e. a GET request to the URI:
+        
+        << STRING12 >> "which represents the Application "
+
+    GET https://<< STRING12 >>/login ?code= << STRING10 >>
+        
+6.  Over at << STRING12 >>, the Application performs the operations, enumerated
+    in (~/io/cognito-oidc-relying-party.js)
+  
+ *
+ *
+ *
+ *
+ *
+ *
+ */
