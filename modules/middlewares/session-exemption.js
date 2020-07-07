981f720131b433bc55e0b8fee82b71c40f6e43a4
jerng-machines <sbx_user1051@169.254.70.173>
2020-07-07 23:20:01 +0000

    -

--- modules/middlewares/session-exemption.js
+++ modules/middlewares/session-exemption.js
@@ -2,26 +2,31 @@
 
 const rus = require('/var/task/modules/r-u-s.js')
 
+/*  This middleware checks the (route) query parameter, and compares it to
+ *  configuration; if (routeValue) is included in (the array of 
+ *  sessionExemptedRouteStrings) of matched by at least one of (the array of
+ *  sessionExemptedRouteRegExps), then (signals.sessionExepted) is set to (true)
+ *  and (signals.skipToMiddlewareName) is specified as 'sessionGuard';
+ *
+ */
+
 const sessionExemption = async(data) => {
 
     if (data.RU.request.queryStringParameters.route &&
         data.RU.request.queryStringParameters.route[0] &&
-        rus.conf.sessionExemptedRoutes.strings.includes ( 
-            data.RU.request.queryStringParameters.route[0]
-        ) && 
+        (rus.conf.sessionExemptedRoutes.strings.includes(
+                data.RU.request.queryStringParameters.route[0]
+            ) ||
+            rus.conf.sessionExemptedRoutes.regExps.some(
+                re => re.test(data.RU.request.queryStringParameters.route[0])
+            )
+
+        )) {
+            
         
-        ) {
-
+        data.RU.signals.sessionExempted = true
+        data.RU.signals.skipToMiddlewareName = 'sessionGuard'
     }
-
-    //  employ this:         data.RU.signals.skipToMiddlewareName = 'composeResponse'
-    //  ... mechanism;
-
-    //////////
-    //      //
-    //  !!  //  Make way.
-    //      //
-    //////////
 
     return data
 }
