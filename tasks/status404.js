ee98937e5a1d3173f566392be7bde2d7480c79b2
jerng-machines <sbx_user1051@169.254.251.69>
2020-07-08 00:48:05 +0000

    -

---
+++ tasks/status404.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const rus = require ( '/var/task/modules/r-u-s.js' )
+
+const status404 = async ( data ) => {
+    data.RU.signals.sendResponse.statusCode = 404
+    data.RU.signals.sendResponse.body = 'HTTP Response Status 404 : Not Found : Unqualified'
+}
+
+module.exports = status404 
+rus.mark ( `~/tasks/status404.js LOADED` )
