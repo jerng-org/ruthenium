c23090e733b67262df847a5ade58aa293bd1e012
jerng-machines <sbx_user1051@169.254.183.125>
2020-07-09 08:08:17 +0000

    -

--- modules/middlewares/oidc-validation.js
+++ modules/middlewares/oidc-validation.js
@@ -28,3 +28,43 @@ const oidcValidation = async(data) => {
 
 module.exports = oidcValidation
 rus.mark(`~/modules/middlewares/oidcValidation.js LOADED`)
+
+/* SAMPLE:
+
+data.RU.signals.
+
+   oidc: {
+      id_token: {
+        at_hash: 'MAz4_Zin9kVmNJX8f0HGGw', // hash of id_token
+        sub: '***REMOVED***', // subject
+        'cognito:groups': [ 'TEST-GROUP' ],
+        email_verified: true,
+        'cognito:preferred_role': 'arn:aws:iam::***REMOVED***:role/***REMOVED***',
+        iss: '***REMOVED***',
+        'cognito:username': '***REMOVED***',
+        'cognito:roles': [
+          'arn:aws:iam::***REMOVED***:role/***REMOVED***'
+        ],
+        aud: '', // relyingparty a.k.a. appID
+        token_use: 'id',
+        auth_time: 1594072214,
+        exp: ,
+        iat: 1594072214,
+        email: '***REMOVED***'
+      },
+      access_token: {
+        sub: '***REMOVED***',// subject
+        'cognito:groups': [ 'TEST-GROUP' ],
+        token_use: 'access',
+        scope: 'aws.cognito.signin.user.admin openid',
+        auth_time: 1594072214,
+        iss: '***REMOVED***',
+        exp: 1594075814,
+        iat: 1594072214,
+        version: 2,
+        jti: '1830597d-346c-42fc-b5af-cd827f192c44', // token ID
+        client_id: '***REMOVED***',
+        username: '***REMOVED***'
+      }
+   }
+*/
