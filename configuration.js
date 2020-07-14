466ce3ef70ce407760817ad31d5fa0183045715f
jerng-machines <sbx_user1051@169.254.26.109>
2020-07-14 17:25:43 +0000

    login link

--- configuration.js
+++ configuration.js
@@ -44,7 +44,7 @@ module.exports = {
      *
      */
     gitCommit: 1,
-    gitCommitMessage: '-',
+    gitCommitMessage: 'login link',
     /*  USES:
      *  Merge order     :   1
      *  Name            :   git-lambda2
