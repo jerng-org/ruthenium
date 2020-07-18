3aae0cd5b08e1b74ed0db72c8133c5203d1b6400
jerng-machines <sbx_user1051@169.254.9.117>
2020-07-18 15:19:59 +0000

    dynamodb notes updated

--- configuration.js
+++ configuration.js
@@ -44,7 +44,7 @@ module.exports = {
      *
      */
     gitCommit: 1,
-    gitCommitMessage: 'login link',
+    gitCommitMessage: 'dynamodb notes updated',
     /*  USES:
      *  Merge order     :   1
      *  Name            :   git-lambda2
