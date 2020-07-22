b1f4db4cd964b3dc8826af6b7d6fa3f4bf84d602
jerng-machines <sbx_user1051@169.254.171.197>
2020-07-22 19:27:10 +0000

    (WIP) restful.js - ()

--- tasks/restful/desks-get-markup.js
+++ tasks/restful/desks-get-markup.js
@@ -10,10 +10,10 @@ const desksGetMarkup = async(data) => {
     let th2s = '<th scope="row">Column Types :</th>'
     let colNames = []
     for (const col of data.RU.io.deskSchemasQuery.Items[0].columns) {
+        colNames.push(col.name)
         th1s += `<th scope="col">${ col.name }</th>`
         th2s += `<th scope="col">${ col.type }</th>`
     }
-    //let rowCount = 1
 
     let deskColumnTypes = {}
     data.RU.io.deskSchemasQuery.Items[0].columns
@@ -25,10 +25,20 @@ const desksGetMarkup = async(data) => {
         if (!(cell.R in deskCells)) {
             deskCells[cell.R] = {}
         }
-        const colName = cell.DHC.slice(cell.D.length + 1)
-        const colType = deskColumnTypes[colName]
-        deskCells[cell.R][colName] = cell[colType]
+        const _colName = cell.DHC.slice(cell.D.length + 1)
+        const _colType = deskColumnTypes[_colName]
+        deskCells[cell.R][_colName] = cell[_colType]
     })
+
+    let tbodyTrs = ''
+    for (const rowID in deskCells) {
+        tbodyTrs += `
+        <tr>
+        ${colNames.reduce((accumulator, colName)=>{
+            return `<td>${deskCells[rowID][colName]}</td>`
+        },'')}
+        </tr>`
+    }
 
     let markup = `
 <h3><i>GET</i> a Desk </h3>
