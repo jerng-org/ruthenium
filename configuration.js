54c6a37fd1d26437c9076e94008ff0ba9cf74bb0
jerng-machines <sbx_user1051@169.254.124.221>
2020-07-22 10:40:12 +0000

    (WIP) restful.js - (sidetrack: fixed a bug in compose-response ... regarding premature initalisation of (data.RU.response) )

--- configuration.js
+++ configuration.js
@@ -45,7 +45,7 @@ module.exports = {
      *
      */
     gitCommit: 1,
-    gitCommitMessage: `(WIP) restful.js - `,
+    gitCommitMessage: `(WIP) restful.js - (sidetrack: fixed a bug in compose-response ... regarding premature initalisation of (data.RU.response) )`,
     /*  USES:
      *  Merge order     :   1
      *  Name            :   git-lambda2
