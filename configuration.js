6d56b9074e3d4529541a6ed0873278fb66b97f5d
jerng-machines <sbx_user1051@169.254.25.29>
2020-08-15 03:44:23 +0000

    TEST-APP-DESK-SCHEMAS now replaced by RUTHENIUM-V1-DESK-SCHEMAS

--- configuration.js
+++ configuration.js
@@ -43,7 +43,7 @@ module.exports = {
      *
      */
     gitCommit: 1,
-    gitCommitMessage: `WIP - NEXT: grep code - simplify (desk-schemas) table by remove (id) - (name) becomes partition key`,
+    gitCommitMessage: `TEST-APP-DESK-SCHEMAS now replaced by RUTHENIUM-V1-DESK-SCHEMAS`,
 //    gitCommitMessage: `NEXT:(desks-get-markup.js) in-line edit / creation / deletion`,
     /*  USES:
      *  Merge order     :   1
