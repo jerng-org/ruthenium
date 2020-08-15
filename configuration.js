29b94aeb51950ce6e8781329975615771f78be74
jerng-machines <sbx_user1051@169.254.165.61>
2020-08-15 02:24:39 +0000

    v1 dev: review (virtual) - NEXT: simplify (desk-schemas) table by remove (id) - (name) becomes partition key

--- configuration.js
+++ configuration.js
@@ -43,8 +43,7 @@ module.exports = {
      *
      */
     gitCommit: 1,
-    gitCommitMessage: '(compose-response.js) data.RU.signals.sendJson behaviour defined',
-//    gitCommitMessage: `v1 dev: review (virtual) - NEXT: simplify (desk-schemas) table by remove (id) - (name) becomes partition key`,
+    gitCommitMessage: `v1 dev: review (virtual) - NEXT: simplify (desk-schemas) table by remove (id) - (name) becomes partition key`,
 //    gitCommitMessage: `NEXT:(desks-get-markup.js) in-line edit / creation / deletion`,
     /*  USES:
      *  Merge order     :   1
