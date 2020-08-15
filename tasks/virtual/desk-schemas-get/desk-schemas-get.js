cb8e260478f6068645eee935274bfabc06ef4890
jerng-machines <sbx_user1051@169.254.165.61>
2020-08-15 02:24:39 +0000

    v1 dev: review (virtual) - NEXT: simplify (desk-schemas) table by remove (id) - (name) becomes partition key

--- tasks/virtual/desk-schemas-get/desk-schemas-get.js
+++ tasks/virtual/desk-schemas-get/desk-schemas-get.js
@@ -1,27 +1,33 @@
 'use strict'
 
-const rus = require ( '/var/task/modules/r-u-s.js' )
+const rus = require('/var/task/modules/r-u-s.js')
 
-const markup 
-    = require ( '/var/task/tasks/virtual/desk-schemas-get/desk-schemas-get-markup.js' )
+const markup = require('/var/task/tasks/virtual/desk-schemas-get/desk-schemas-get-markup.js')
 rus.conf.verbosity > 0 &&
     console.warn(`(desk-schemas-get.js) FIXME: rendering (-markup.js) should not involve a require() here;`)
 
-const deskSchemasGet = async ( data ) => {
+const deskSchemasGet = async(data) => {
 
-    data.RU.io.deskSchemasScan = await rus.aws.ddbdc.scan ( {
+    data.RU.io.deskSchemasScan = await rus.aws.ddbdc.scan({
         TableName: 'TEST-APP-DESK-SCHEMAS',
-        ReturnConsumedCapacity : 'TOTAL'
-    } ).promise()
+        ReturnConsumedCapacity: 'TOTAL'
+    }).promise()
 
-    switch ( data.RU.request.queryStringParameters.reader[0] ) {
-        
-        case ( 'machine' ) :
-            data.RU.signals.sendJson = data.RU.io.deskSchemasScan 
+    switch (data.RU.request.queryStringParameters.reader[0]) {
+
+        case ('machine'):
+            data.RU.signals.sendJson = data.RU.io.deskSchemasScan
+            data.RU.signals.sendJson = data.RU.signals.sendJson.Items.forEach(
+                (currentValue, index, array) => {
+                    
+                    // TEMPORARY: for migration of data 2020-08-15 only
+                    delete currentValue.id
+                } /*, thisArg*/
+            )
             break
-        case ( 'human' ) :
+        case ('human'):
         default:
-            data.RU.signals.sendResponse.body = await markup ( data )
+            data.RU.signals.sendResponse.body = await markup(data)
     }
 
 }
