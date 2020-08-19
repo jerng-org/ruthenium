db0702fdcd2521757c90fcb6e6defb40b9a85b7c
jerng-machines <sbx_user1051@169.254.220.141>
2020-08-19 22:02:30 +0000

    (initial) : (html.js) improved

--- tasks/virtual/desk-schemas-get/markup-li-of-columns-in-tr.js
+++ tasks/virtual/desk-schemas-get/markup-li-of-columns-in-tr.js
@@ -7,7 +7,7 @@ const liOfColumnsInTr = async ( column ) => {
                     class="button-outline " 
                     onclick="return false;"> 
                 <span class="toggle-set-1">
-                    <i class="material-icons">lock (stub) </i>rename</span>
+                    <i class="material-icons">lock</i>rename (stub) </span>
             </button>
             <button class="button-outline" onclick="return false;">
                 <i class="material-icons">lock</i>destroy (stub) </button> 
