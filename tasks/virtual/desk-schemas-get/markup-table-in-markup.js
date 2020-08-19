39c3825a431f29633df311dcdfe872f5471e7a60
jerng-machines <sbx_user1051@169.254.49.213>
2020-08-19 20:34:26 +0000

    (initial) : notes on architecture

--- tasks/virtual/desk-schemas-get/markup-table-in-markup.js
+++ tasks/virtual/desk-schemas-get/markup-table-in-markup.js
@@ -10,48 +10,7 @@ const tableInMarkup = async ( deskSchemasScan ) => {
                 <th colspan="2">
                 
                     <p>System is currently aware of ${ deskSchemasScan.Count } Desk Schemas</p>
-                    <h6>
-                        <div style="color:#f00; font-weight:700;">
-                            <p>Current priority:</p>
-                            <ul>
-                                <li>
-                                    <p>does routing look okay?</p>
-                                    <p>
-                                        <pre>
-                                            We're currently working with something that looks like:
-                                            
-                                                - ? route=virtual
-                                                - & type=(desk-schemas, or desks)
-                                                - & thing=(UNDEFINED-for-desk-schemas, or DESK-NAME-for-desks)
-                                                - & reader=(human, or machine)
-                                            
-                                            We could migrate to:
-                                            
-                                                -   "storage=virtual & type=Deskname     & thing=Rowid"
-                                                -   "storage=actual  & type=desk-schemas & thing=Deskname"
-                                                -   "storage=actual  & type=desk-cells   & thing=Deskname#Columnname,Rowid"
-    
-                                                (we've stopped caring if "type" and "Columnname" are singular or plural)        
-                                                
-                                                User-story:     
-                                                
-                                                1.  Define traits for desks, in desk-schemas 
-                                                2.  All desks which share a desk-schema trait all work the same way
-                                                
-                                            But we really shouldn't bother until v2.
-                                        </pre>
-                                    </p>
-                                </li>
-                                <li>does CRUD for desk-schemas work?</li>
-                                <li>does CRUD for desks work?</li>
-                                <li>what's missing?</li>
-                                <li></li>
-                                <li></li>
-                                <li></li>
-                                <li></li>
-                            </ul>
-                        </div>
-                    </h6>
+
                     <a class="button float-right" href="#">Create Desk</a>
     
                 </th>
