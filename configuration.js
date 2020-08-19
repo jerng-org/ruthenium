dd9563e03c62ceda01c40d8827d8a16efe814703
jerng-machines <sbx_user1051@169.254.89.101>
2020-08-19 20:47:30 +0000

    (initial) : (html.js) improved

--- configuration.js
+++ configuration.js
@@ -43,7 +43,7 @@ module.exports = {
      *
      */
     gitCommit: 1,
-    gitCommitMessage: `(initial) : notes on architecture` ,
+    gitCommitMessage: `(initial) : (html.js) improved` ,
     /*  USES:
      *  Merge order     :   1
      *  Name            :   git-lambda2
