d256349c03b8a8ec711eefba4c8869d32ad14bdc
jerng-machines <sbx_user1051@169.254.66.21>
2020-08-21 23:00:09 +0000

    (virtual.js) : defined (deskSchemasGetSuccess)

--- tasks/virtual/forms-get/markup-delete-desk-schema.js
+++ tasks/virtual/forms-get/markup-delete-desk-schema.js
@@ -7,16 +7,10 @@ const deleteDeskSchema = async ( data ) => {
     return `
     
         <h2>Desk Schema : <code>delete one</code> </h2>
-    
-        ${  await rus.html.form ( {
-                action: await rus.appUrl( [ 
-                    [ 'route','virtual' ], 
-                    [ 'type','desk-schemas' ] 
-                ] ),
-                innerHTML: '-'
-            } ) 
-        }
-    `
-    
+        
+        <pre><code>${ await rus.print.stringify4 ( data.RU.io.deskSchemasGet.Item ) }</code></pre>
+        
+        `
+        
 }
 module.exports = deleteDeskSchema
