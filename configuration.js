bddb8a8b482a107947e34af5a555aeb763021027
jerng-machines <sbx_user1051@169.254.55.117>
2020-08-21 18:45:40 +0000

    (virtual.js) : defined (deskSchemasGetSuccess)

--- configuration.js
+++ configuration.js
@@ -43,7 +43,7 @@ module.exports = {
      *
      */
     gitCommit: 1,
-    gitCommitMessage: `(virtual.js) : prepared to check database for ?desk-schema-name` ,
+    gitCommitMessage: `(virtual.js) : defined (deskSchemasGetSuccess)` ,
     /*  USES:
      *  Merge order     :   1
      *  Name            :   git-lambda2
