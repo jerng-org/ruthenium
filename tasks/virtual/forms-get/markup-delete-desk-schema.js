df50eada26baddad7403628c18d443c694bffd0f
jerng-machines <sbx_user1051@169.254.247.237>
2020-08-21 23:02:30 +0000

    (virtual.js) : -

--- tasks/virtual/forms-get/markup-delete-desk-schema.js
+++ tasks/virtual/forms-get/markup-delete-desk-schema.js
@@ -8,8 +8,10 @@ const deleteDeskSchema = async ( data ) => {
     
         <h2>Desk Schema : <code>delete one</code> </h2>
         
-        <h3>The following item was deleted from the server. Please save this
-        transcription if you wish to reimport it later.</h3>
+        <h3>
+            The following item was deleted from the server.<br>
+            Please save this transcription if you wish to reimport it later.
+        </h3>
         
         <pre><code>${ await rus.print.stringify4 ( data.RU.io.deskSchemasGet.Item ) }</code></pre>
         
