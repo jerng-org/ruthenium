a2035d73ae1d3ee8463c16577971a8d1166576ed
jerng-machines <sbx_user1051@169.254.48.189>
2020-08-21 19:37:03 +0000

    (virtual.js) : defined (deskSchemasGetSuccess)

--- tasks/virtual/forms-get/markup-read-desk-schema.js
+++ tasks/virtual/forms-get/markup-read-desk-schema.js
@@ -10,25 +10,25 @@ const readDeskSchema = async ( data ) => {
         
         <h3><code>pre</code></h3>
         <pre>
-            ${ rus.print.stringify4 ( data.RU.io.deskSchemasGet ) }
+            ${ await rus.print.stringify4 ( data.RU.io.deskSchemasGet ) }
         </pre>    
         
         <h3><code>code</code></h3>
         <code>
-            ${ rus.print.stringify4 ( data.RU.io.deskSchemasGet ) }
+            ${ await rus.print.stringify4 ( data.RU.io.deskSchemasGet ) }
         </code>    
 
         <h3><code>pre > code</code></h3>
         <pre>
         <code>
-            ${ rus.print.stringify4 ( data.RU.io.deskSchemasGet ) }
+            ${ await rus.print.stringify4 ( data.RU.io.deskSchemasGet ) }
         </code>
         </pre>    
         
         <h3><code>code > pre</code></h3>
         <code>
         <pre>
-            ${ rus.print.stringify4 ( data.RU.io.deskSchemasGet ) }
+            ${ await rus.print.stringify4 ( data.RU.io.deskSchemasGet ) }
         </pre>    
         </code>
     `
