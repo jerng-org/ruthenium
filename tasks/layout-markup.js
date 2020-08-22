163c140652e9776a9ba48e305ea789e01f26f144
jerng-machines <sbx_user1051@169.254.173.13>
2020-08-22 02:01:34 +0000

    (virtual.js) : -

--- tasks/layout-markup.js
+++ tasks/layout-markup.js
@@ -76,6 +76,9 @@ await rus.appUrl ( [
                 Array.prototype.forEach.call ( 
                     scopingElement.querySelectorAll( classString ), 
                     e => {
+                    
+                        console.log('toggler classString element', e)
+                    
                         const wasVisible = ['','initial'].includes(e.style.display)
                         e.style.display = wasVisible ? 'none' : ''
                         
