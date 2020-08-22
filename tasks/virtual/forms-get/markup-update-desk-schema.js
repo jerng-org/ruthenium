8bb4c949d2af0894bf962f670c4e93d6a4b8f4b1
jerng-machines <sbx_user1051@169.254.197.181>
2020-08-22 04:09:50 +0000

    (markup-tr-in-table.js) C/R/D settled

--- tasks/virtual/forms-get/markup-update-desk-schema.js
+++ tasks/virtual/forms-get/markup-update-desk-schema.js
@@ -3,6 +3,98 @@
 const rus = require ( '/var/task/modules/r-u-s.js' )
 
 const updateDeskSchema = async ( data ) => {
+
+
+const innerHTML = async () => `
+<fieldset>
+
+    ${ await rus.html.input ( {
+        name:       `desk-schemas[name]`,
+        id:         `desk-schemas[name]`,
+        value:      data.RU.io.deskSchemasGet.Item.name,
+        required:   true,
+        label:      `Name of the schema :`
+    } ) }
+    
+    <table  data-ru-incrementable-group="column-definition"
+            data-ru-incrementable-role="parent"
+            >
+        <tr>
+            <td>
+                <i  class="material-icons"
+                    data-ru-incrementable-group="column-definition"
+                    data-ru-incrementable-role="append-one""
+                    data-ru-incrementable-attributes='[
+                        {   
+                            "attribute":    "name",
+                            "baseValue":    "desk-schemas[columns]###[name]"
+                        },
+                        {   
+                            "attribute":    "name",
+                            "baseValue":    "desk-schemas[columns]###[type]"
+                        }
+                    ]'
+                    >
+                    add_circle_outline</i>
+            
+            </td>
+            <td>
+                <label>
+                    Add columns to the schema :
+                    <h6>&nbsp;</h6>
+                    </label>
+            </td>
+            <td>
+                <label>
+                    Specify column type :
+                    <h6>'string' and 'number' are efficient</h6> 
+                    </label>
+            </td>
+        </tr>
+
+<!---------------------------------------------------------------------------->        
+        <template data-ru-incrementable-group="column-definition">
+        <tr data-ru-incrementable-group="column-definition"
+            data-ru-incrementable-role="appended-child"
+            >
+            <td>
+                <i  class="material-icons"stub
+                    data-ru-incrementable-group="column-definition"
+                    data-ru-incrementable-role="remove-closest"
+                    >
+                    remove_circle_outline</i>
+            </td>
+            <td>
+                ${ await rus.html.input ( {
+                    name:       `desk-schemas[columns]###[name]`,
+                    placeholder:`-- enter a column name --`,
+                    required:   true
+                } ) }
+            </td>
+            <td>
+                <select name="desk-schemas[columns]###[type]"
+                        required
+                        >
+                    <option disabled selected value> -- select an option -- </option>
+                    <option value="S">string</option>
+                    <option value="N">number</option>
+                    <option value="other">other</option>
+                    </select>
+            </td>
+        </tr>
+        </template>
+<!---------------------------------------------------------------------------->        
+        
+    </table>
+    
+    ${  await rus.html.input ( {
+        value:  `POST it`,
+        type:   `submit`
+    } ) }
+
+</fieldset>
+`
+
     
     return `
     
