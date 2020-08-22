1b3105e533063f454e5def95350e5dd89dc5f0f2
jerng-machines <sbx_user1051@169.254.129.141>
2020-08-22 01:38:11 +0000

    (virtual.js) : -

--- tasks/virtual/desk-schemas-get/markup-tr-in-table.js
+++ tasks/virtual/desk-schemas-get/markup-tr-in-table.js
@@ -199,7 +199,8 @@ const trInTable = async(item) => {
                         style="display:none;"
                         title="delete schema forever"
                         id="desk-schemas-delete-${ item['name'] }"
-                        onblur="(e => console.log(e.target))(event)"
+                        onfocus="(e => console.log('focused', e.target))(event)"
+                        onblur="(e => console.log('blurred', e.target))(event)"
                         href="${
                     
                         await rus.appUrl ([
