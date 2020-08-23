d0c12d790358d9ca8db45fedc7a653c2c67f619f
jerng-machines <sbx_user1051@169.254.205.133>
2020-08-23 00:39:15 +0000

    -

--- tasks/status-400.js
+++ tasks/status-400.js
@@ -5,7 +5,7 @@ const rus = require ( '/var/task/modules/r-u-s.js' )
 const status400 = async ( data ) => {
     data.RU.signals.sendResponse.statusCode = 400
     data.RU.signals.sendResponse.body = 
-        '<h2>HTTP Response Status 400 : Bad Request</h2><h3>What were you trying to do?</h3>'
+        'HTTP Response Status Code <h2>400 Bad Request</h2><h3>What were you trying to do?</h3>'
 }
 
 module.exports = status400 
--- tasks/status-422.js
+++ tasks/status-422.js
@@ -4,7 +4,7 @@ const rus = require ( '/var/task/modules/r-u-s.js' )
 
 const status422 = async ( data ) => {
     data.RU.signals.sendResponse.statusCode = 422
-    data.RU.signals.sendResponse.body = '<h2>HTTP Response Status 422 : Unprocessable Entity</h2><h3>Syntax understood; Semantics ... not so much.</h3>'
+    data.RU.signals.sendResponse.body = 'HTTP Response Status Code <h2>422 Unprocessable Entity</h2><h3>Syntax understood; Semantics ... not so much.</h3>'
 }
 
 module.exports = status422
