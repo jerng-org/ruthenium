ffabf822fb02fd7490135e11a5d13158981c6601
jerng-machines <sbx_user1051@169.254.205.133>
2020-08-23 00:39:15 +0000

    -

--- tasks/status-404.js
+++ tasks/status-404.js
@@ -4,7 +4,7 @@ const rus = require ( '/var/task/modules/r-u-s.js' )
 
 const status404 = async ( data ) => {
     data.RU.signals.sendResponse.statusCode = 404
-    data.RU.signals.sendResponse.body = '<h2>HTTP Response Status 404 : Not Found</h2><h3>What were you looking for?</h3>'
+    data.RU.signals.sendResponse.body = 'HTTP Response Status Code<h2>404 Not Found</h2><h3>What were you looking for?</h3>'
 }
 
 module.exports = status404
--- tasks/status-501.js
+++ tasks/status-501.js
@@ -5,7 +5,7 @@ const rus = require ( '/var/task/modules/r-u-s.js' )
 const status501 = async ( data ) => {
     data.RU.signals.sendResponse.statusCode = 501
     data.RU.signals.sendResponse.body = 
-    '<h2>HTTP Response Status 501 : Not Implemented</h2><h3>Please report this to the system administrator.</h3>'
+    'HTTP Response Status Code <h2>501 Not Implemented</h2><h3>Please report this to the system administrator.</h3>'
 }
 
 module.exports = status501 
