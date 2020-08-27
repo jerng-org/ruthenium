e7d09c3c4c0ed13e40ae67ce2acf7e5e72eca8ca
jerng-machines <sbx_user1051@169.254.162.173>
2020-08-27 15:20:09 +0000

    -

--- tasks/virtual/desks-get-markup.js
+++ tasks/virtual/desks-get-markup.js
@@ -6,16 +6,14 @@ const rus = require('/var/task/modules/r-u-s.js')
 
 const desksGetMarkup = async(data) => {
 
-    let th1s = '<th scope="row" >Property Name</th>'
-    let th2s = '<th scope="row" >Property Type</th>' 
-    let th3s = `<th scope="col" colspan="999">
+    let th1s = '<th scope="row" >Property Name<sub>Property Type</sub></th>'
+    let th2s = `<th scope="col" colspan="999">
                         <code>${ data.RU.io.deskSchemasQuery.Items[0].name }</code>Object Count (click to show ID)
                     </th>`
     let colNames = []
     for (const col of data.RU.io.deskSchemasQuery.Items[0].columns) {
         colNames.push(col.name)
         th1s += `<th scope="col">${ col.name }<sub>${ rus.conf.labels.deskCellTypes[col.type] }</sub></th>`
-        th2s += `<th scope="col"></th>`
     }
 
     th1s += `<th scope="col" rowspan="2">Operations on each Object</th>`
@@ -182,7 +180,6 @@ const desksGetMarkup = async(data) => {
         </tr>
         <tr>${ th1s }</tr>        
         <tr>${ th2s }</tr>        
-        <tr>${ th3s }</tr>        
     </thead>
     
     <tbody>${ tbodyTrs }</tbody>
