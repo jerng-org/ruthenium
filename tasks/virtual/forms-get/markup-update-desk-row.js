46ae85a88194e5a97089ea646336ec6d45c9d44d
jerng-machines <sbx_user1051@169.254.131.37>
2020-09-20 19:58:30 +0000

    (virtual/desks-patch.js: sub-PUTs are working, DELETEs are not;

---
+++ tasks/virtual/forms-get/markup-update-desk-row.js
@@ -0,0 +1,80 @@
+'use strict'
+
+const rus = require('/var/task/modules/r-u-s.js')
+
+const innerHTML = async(DATA, uuid) => `
+<fieldset>
+
+    ${  
+        await DATA.RU.io.deskSchemasGet.Item.columns.reduce( 
+            async ( accumulator, currentValue, index, array ) => {
+                return await accumulator + 
+                    await rus.html.input ( {
+                        id:     `id`,
+                        name:   `desk-cells[PUT]###${index}###[R]`,
+                        type:   `hidden`,
+                        value:  uuid
+                    } ) +
+                    await rus.html.input ( {
+                        id:             currentValue.name,
+                        label:          currentValue.name,
+                        name:           `
+desk - cells[PUT]### $ { index }###[$ { currentValue.type }]
+`,
+                        placeholder:    `--enter a $ { rus.conf.labels.deskCellTypes[currentValue.type] }--`
+                        
+                    } ) +
+                    await rus.html.input ( {
+                        type:   'hidden',
+                        name:   `
+desk - cells[PUT]### $ { index }###[DHC]
+`,
+                        value:  DATA.RU.io.deskSchemasGet.Item.name + '#' + currentValue.name
+                    } ) +
+                    await rus.html.input ( {
+                        type:   'hidden',
+                        name:   `
+desk - cells[PUT]### $ { index }###[D]
+`,
+                        value:  DATA.RU.io.deskSchemasGet.Item.name
+                    } )
+            },
+            
+            `
+` /* initial value (second argument) */
+            
+        )
+    } 
+
+    <button type="submit"
+            title="submit form: create object"
+            >
+            <i class='material-icons'>send</i> SUBMIT FORM</button>
+                        
+                        
+</fieldset>
+`
+
+const updateDeskRow = async(data) => {
+      
+    const newUuid = await rus.uuid4()
+    
+    return `
+    
+        <h2><code>${ data.RU.io.deskSchemasGet.Item.name }</code> : object creation </h2>
+    
+        ${  await rus.html.form ( {
+                action: await rus.appUrl( [
+                    [ 'route','virtual' ], 
+                    [ 'type','desks' ], 
+                    [ 'thing', data.RU.io.deskSchemasGet.Item.name ], 
+                    [ 'form-method','PATCH' ] 
+                ] ),
+                innerHTML: await innerHTML(data, newUuid),
+                class: 'ru-card'
+            } ) 
+        }
+    `
+      
+}
+module.exports = updateDeskRow
