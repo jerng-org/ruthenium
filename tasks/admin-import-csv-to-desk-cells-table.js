38226f2bcba540b08a2301070481d956d7c1d534
jerng-machines <sbx_user1051@169.254.191.181>
2020-09-30 17:00:37 +0000

    WIP: (textarea validation done) <= housekeeping task to upload CSV to DESKS

--- tasks/admin-import-csv-to-desk-cells-table.js
+++ tasks/admin-import-csv-to-desk-cells-table.js
@@ -85,6 +85,28 @@ Desk cells
             }
             return ret;
         };
+        
+        function parseCsv(data, fieldSep, newLine) {
+            fieldSep = fieldSep || ',';
+            newLine = newLine || '\\n';
+            var nSep = '\\x1D';
+            var qSep = '\\x1E';
+            var cSep = '\\x1F';
+            var nSepRe = new RegExp(nSep, 'g');
+            var qSepRe = new RegExp(qSep, 'g');
+            var cSepRe = new RegExp(cSep, 'g');
+            var fieldRe = new RegExp('(?<=(^|[' + fieldSep + '\\\\n]))&quot;(|[\\\\s\\\\S]+?(?<![^&quot;]&quot;))&quot;(?=($|[' + fieldSep + '\\\\n]))', 'g');
+            var grid = [];
+            data.replace(/\\r/g, '').replace(/\\n+$/, '').replace(fieldRe, function(match, p1, p2) {
+                return p2.replace(/\\n/g, nSep).replace(/&quot;&quot;/g, qSep).replace(/,/g, cSep);
+            }).split(/\\n/).forEach(function(line) {
+                var row = line.split(fieldSep).map(function(cell) {
+                    return cell.replace(nSepRe, newLine).replace(qSepRe, '&quot;').replace(cSepRe, ',');
+                });
+                grid.push(row);
+            });
+            return grid;
+        }
 
     </script>
 `,
@@ -95,7 +117,7 @@ console.log('--start onkeyup--')
 const textarea = document.getElementById('desk-cells-as-csv');
 const textareaValue = textarea.value;
 const outputElement = document.getElementById('desk-cells-as-csv-validity');
-const csvAsArray = csvToArray(textareaValue);
+const csvAsArray = csvParse(textareaValue);
 outputElement.innerText = JSON.stringify(csvAsArray,null,4);
 console.log('--end onkeyup--')
 ` 
