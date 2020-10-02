fefe65a01d7cf23d2d530a8b8a012564c8049eed
jerng-machines <sbx_user1051@169.254.161.5>
2020-10-02 04:50:32 +0000

    WIP: (CSV validator upgraded with space-padded quotes option; NEXT: schema name select element) <= housekeeping task to upload CSV to DESKS

---
+++ tasks/uuid4-create.js
@@ -0,0 +1,20 @@
+'use strict'
+
+//  Provide a debuggable function name, 
+//  in order to avoid debuggin (function).toString()
+
+const rus = require ( '/var/task/modules/r-u-s.js' )
+
+const uuid4Create = async ( data ) => {
+
+    data.RU.signals.noLayout = true
+    
+    data.RU.response.body = rus.uuid4()
+
+    rus.mark ( `~/tasks/_template-task.js EXECUTED` )
+}
+// manipulate (data.RU), for example
+// no need to return (data)
+
+module.exports = uuid4Create
+rus.mark ( `~/tasks/uuid4Create.js LOADED` )
