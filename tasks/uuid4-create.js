f0987c46d695dbdb9bc68f9fc9893460b4c1bbb3
jerng-machines <sbx_user1051@169.254.157.69>
2020-10-02 05:05:27 +0000

    WIP: (CSV validator upgraded with space-padded quotes option; NEXT: schema name select element) <= housekeeping task to upload CSV to DESKS

--- tasks/uuid4-create.js
+++ tasks/uuid4-create.js
@@ -3,18 +3,22 @@
 //  Provide a debuggable function name, 
 //  in order to avoid debuggin (function).toString()
 
-const rus = require ( '/var/task/modules/r-u-s.js' )
+const rus = require('/var/task/modules/r-u-s.js')
 
-const uuid4Create = async ( data ) => {
+const uuid4Create = async(data) => {
 
     data.RU.signals.noLayout = true
-    
-    data.RU.signals.sendResponse = { body : await rus.uuid4() }
 
-    rus.mark ( `~/tasks/_template-task.js EXECUTED` )
+    const quantity = data.RU.request.queryStringParameters.quantity ?
+        data.RU.request.queryStringParameters.quantity :
+        1
+
+    data.RU.signals.sendResponse = { body: Array.from ( Array( quantity )).map( _ => await rus.uuid4() ) } 
+
+    rus.mark(`~/tasks/_template-task.js EXECUTED`)
 }
 // manipulate (data.RU), for example
 // no need to return (data)
 
 module.exports = uuid4Create
-rus.mark ( `~/tasks/uuid4Create.js LOADED` )
+rus.mark(`~/tasks/uuid4Create.js LOADED`)
