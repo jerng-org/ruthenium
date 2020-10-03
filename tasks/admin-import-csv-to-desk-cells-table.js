f876121c867e27f2ab8cac06e0b4bcd3d17f9457
jerng-machines <sbx_user1051@169.254.182.229>
2020-10-03 02:28:30 +0000

    WIP: (uuid4-create.js; CURRENT: onsubmit callback - formData constructionnnnn; NEXT: schema name select element) <= housekeeping task to upload CSV to DESKS

--- tasks/admin-import-csv-to-desk-cells-table.js
+++ tasks/admin-import-csv-to-desk-cells-table.js
@@ -36,6 +36,9 @@ ${ await rus.html.form ( {
     const formData = new FormData()
     const deskSchemasName = document.getElementById('desk-schemas[name]').value
     const parseResults = document.getElementById('desk-cells-as-csv')._parseResults
+    const newUuid4s = await fetch ( '/?route=uuid4-create', {
+        
+    } ) 
     let cellIndex = 0
     
     {
