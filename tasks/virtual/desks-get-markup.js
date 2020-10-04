b6488526785627b3e3895284d93c9f0b77cc782b
jerng-machines <sbx_user1051@169.254.149.125>
2020-10-04 22:17:39 +0000

    CSS added: dialog::backdrop

--- tasks/virtual/desks-get-markup.js
+++ tasks/virtual/desks-get-markup.js
@@ -218,7 +218,7 @@ const desksGetMarkup = async(data) => {
 }
 
 let markup = `
-<dialog>
+<dialog onclick="event.stopImmediatePropagation()">
     <script>
         const tempPointer = document.getElementsByTagName('dialog')[0]
         document.getElementsByTagName('body')[0].onclick = _ => {
