2eb411340e40f584b846d90f65bce866130fde0d
jerng-machines <sbx_user1051@169.254.214.181>
2020-12-13 16:49:44 +0000

    (migrated to theunicorn.coffee); WIP: desks-get-markup.js: cell by cell updates

--- configuration.js
+++ configuration.js
@@ -1,5 +1,5 @@
 // Dev: easy to find and edit
-const gitCommitMessage = `[fn:ruthenium-v1-dev_THEUNICORN]; WIP: desks-get-markup.js: cell by cell updates`
+const gitCommitMessage = `(migrated to theunicorn.coffee); WIP: desks-get-markup.js: cell by cell updates`
 
 
 module.exports = {
