8cfe3661680df7c3ddf81be430f0fb0880d87ff4
jerng-machines <sbx_user1051@169.254.108.237>
2020-12-17 18:24:24 +0000

    Added 'sharp' node module layer; WIP: desks-get-markup.js: cell by cell updates

--- configuration.js
+++ configuration.js
@@ -1,5 +1,5 @@
 // Dev: easy to find and edit
-const gitCommitMessage = `WIP: desks-get-markup.js: cell by cell updates`
+const gitCommitMessage = `Added 'sharp' node module layer; WIP: desks-get-markup.js: cell by cell updates`
 
 
 module.exports = {
