ea2beac508cbcc5c6034115db1bb85a7e8fdf872
jerng-machines <sbx_user1051@169.254.171.69>
2021-01-02 17:33:33 +0000

    WIP: s3.js; route=s3-post-policy-test; WIP: desks-get-markup.js: cell by cell updates

---
+++ tasks/s3-post-policy-test.js
@@ -0,0 +1,21 @@
+'use strict'
+
+//  Provide a debuggable function name, 
+//  in order to avoid debuggin (function).toString()
+
+const rus = require ( '/var/task/modules/r-u-s.js' )
+
+const s3PostPolicyTest = async ( data ) => {
+
+    // YOUR CODE HERE
+    
+    // set data in ( data.RU.io.thisIsMyName )
+
+
+    rus.mark ( `~/tasks/s3-post-policy-test.js EXECUTED` )
+}
+// manipulate (data.RU), for example
+// no need to return (data)
+
+module.exports = s3PostPolicyTest
+rus.mark ( `~/tasks/s3-post-policy-test.js LOADED` )
