e1e214b1ae311563f6ef69355587f6491e48f10c
jerng-machines <sbx_user1051@169.254.180.245>
2021-06-12 19:56:44 +0000

    comments

--- configuration.js
+++ configuration.js
@@ -1,6 +1,6 @@
 // Dev: easy to find and edit
 const gitCommit = 1
-const gitCommitMessage = 'comment; and ergonomic variable addition' //`WIP: s3.js; route=s3-post-policy-test; WIP: desks-get-markup.js: cell by cell updates`
+const gitCommitMessage = `comments`//`WIP: s3.js; route=s3-post-policy-test; WIP: desks-get-markup.js: cell by cell updates`
 
 module.exports = {
 
