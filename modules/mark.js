fab41230de494a989b2f5804f0cf2d4c9c329cd0
jerng-machines <sbx_user1051@169.254.14.93>
2022-04-25 18:52:33 +0000

    debugged : custom logger

--- modules/mark.js
+++ modules/mark.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const conf = require(`/var/task/configuration.js`)
+const customLogger = require(`/var/task/modules/custom-logger.js`)
 
 const _log = conf.performance ? console.log : _ => _
 
