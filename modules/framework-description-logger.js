1f37354c5f79a2e0fa39a3e106d3ce3f7f462020
jerng-machines <sbx_user1051@169.254.93.173>
2022-04-27 17:49:56 +0000

    adding depth to frameworkDescriptionLogger

--- modules/framework-description-logger.js
+++ modules/framework-description-logger.js
@@ -10,12 +10,13 @@ const conf = require(`/var/task/configuration.js`)
 
 var frameworkDescriptionLogger = {
     frameworkDescriptionLogString: '',
+    currentFunctionDescription: undefined,
+    log: undefined,
+    endLog: undefined,
     callDepth: 0,
     callEnumeration: [],
     callStarts: undefined,
     callEnds: undefined,
-    log: undefined,
-    endLog: undefined
 }
 
 if (conf.frameworkDescriptionLogging) {
@@ -23,27 +24,30 @@ if (conf.frameworkDescriptionLogging) {
     frameworkDescriptionLogger.frameworkDescriptionLogString =
         "\n\nFrameworkDescriptionLogString STARTED (framework-description-logger.js INITIALISATION)"
 
-    frameworkDescriptionLogger.callStarts = _ => {
-        frameworkDescriptionLogger.callDepth++
-
-    }
-    
-    frameworkDescriptionLogger.callEnds = _ => {
-        frameworkDescriptionLogger.callDepth--
-
-    }
-
     frameworkDescriptionLogger.endLog = _ => {
         frameworkDescriptionLogger.frameworkDescriptionLogString +=
             '\n|\n| ... FrameworkDescriptionLogString ENDED (framework-description-logger.js/endLog EXECUTION)\n'
     }
 
     frameworkDescriptionLogger.log = _input => {
-        frameworkDescriptionLogger.frameworkDescriptionLogString += 
-        ('\n|\n|' + _input).replace(/\n/g,'\n' + '... '.repeat(frameworkDescriptionLogger.callDepth))
+        frameworkDescriptionLogger.frameworkDescriptionLogString +=
+            ('\n|\n|' + _input).replace(/\n/g, '\n' + '... '.repeat(frameworkDescriptionLogger.callDepth))
         console.log(frameworkDescriptionLogger.frameworkDescriptionLogString)
     }
-    
+
+    frameworkDescriptionLogger.callStarts = _currentFunctionDescription => {
+        frameworkDescriptionLogger.callDepth++
+        frameworkDescriptionLogger.log('Starting execution (' + _currentFunctionDescription + ')')
+        frameworkDescriptionLogger.currentFunctionDescription = _currentFunctionDescription
+    }
+
+    frameworkDescriptionLogger.callEnds = _ => {
+        frameworkDescriptionLogger.callDepth--
+        frameworkDescriptionLogger
+            .log('Ending execution (' + frameworkDescriptionLogger.currentFunctionDescription + ')')
+        delete frameworkDescriptionLogger.currentFunctionDescription 
+    }
+
 }
 else {
     frameworkDescriptionLogger.log = _ => _
