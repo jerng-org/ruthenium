0b5bc2ac9a3525e2e5459a0e4881bb3200b95370
jerng-machines <sbx_user1051@169.254.92.181>
2023-05-30 14:53:36 +0000

    review, refactoring, & documentation : custom-logger

--- configuration.js
+++ configuration.js
@@ -1,6 +1,6 @@
 'use strict'
 // Dev: easy to find and edit
-const gitCommit = 1 
+const gitCommit = 1
 const gitCommitMessage = `review, refactoring, & documentation : custom-logger`
 //`WIP: s3.js; route=s3-post-policy-test; WIP: desks-get-markup.js: cell by cell updates`
 
@@ -43,6 +43,28 @@ module.exports = {
     customLogging: 1,
     customLoggingAllowsNativeLogging: 0,
 
+    /*  dateTimeFormat 
+     *  
+     *  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat#locales 
+     */ 
+     
+    dateTimeFormat: new Intl.DateTimeFormat(
+        module.exports.dateTimeFormatBcp47Tag,
+        module.exports.dateTimeFormatOptions
+    ),
+    dateTimeFormatBcp47Tag: 'sv',
+    dateTimeFormatOptions: {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        fractionalSecondDigits: 3,
+        timeZoneName: 'short',
+        timeZone: module.exports.ianaTimeZone
+    },
+
     defaults: {
 
         cookie: {
@@ -70,7 +92,7 @@ module.exports = {
      *
      *      3   :   2 + verbose narration
      */
-    frameworkDescriptionLogging : 0,
+    frameworkDescriptionLogging: 0,
 
     /*  gitCommit
      *
@@ -88,6 +110,8 @@ module.exports = {
      *  Layer version   :   6
      *  Version ARN     :   arn:aws:lambda:us-east-1:553035198032:layer:git-lambda2:6
      */
+
+    ianaTimeZone: 'Asia/Kuala_Lumpur',
 
     labels: {
 
