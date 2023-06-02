e4d8b97b70f427f1da55670416c19c2bbfc9873e
jerng-machines <sbx_user1051@169.254.88.237>
2023-06-02 22:20:52 +0000

    review, refactoring, & documentation : streamline customLogging, frameworkDescriptionLogging, mark : stack traces

--- modules/custom-logger.js
+++ modules/custom-logger.js
@@ -20,23 +20,23 @@ const customLogger = {
 if (conf.customLogging) {
 
     const customLogStringDate = new Date
-    
+
     let result
     let err = {}
     Error.captureStackTrace(err)
-    
+
     const buildLineStyle1 = (_continue, _arguments, _postDateLabel) =>
         customLogger.customLogString += "\n" +
         conf.dateTimeFormat.format(customLogStringDate) + _postDateLabel + e1 +
         (
-            (result = err.stack.match(/\n.*\n.*at (.*)\n?/)) ?
-            result[1] :
+            /* (result = err.stack.match(/\n.*\n.*at (.*)\n?/)) ?
+            result[1] : */
             err.stack
         ) +
         Array.from(_arguments)
         .join('\n')
         .replace(/\n/g, '\n' + ' '.repeat(35) + e1)
-        
+
     const buildLineStyle2 = (_continue, _arguments, _postDateLabel, _postEmoji) =>
         customLogger.customLogString = (_continue ?
             customLogger.customLogString + '\n' :
