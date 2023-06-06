1076318b202700ce323c5d158066463b73815fb9
jerng-machines <sbx_user1051@169.254.107.141>
2023-06-06 17:09:12 +0000

    comments : classification

--- configuration.js
+++ configuration.js
@@ -1,8 +1,7 @@
 'use strict'
 // Dev: easy to find and edit
 const _gitCommit = 1
-const _gitCommitMessage = 'remove redundant (mark) labels; touch2'
-//`comments : classification `
+const _gitCommitMessage = `comments : classification `
 //`WIP: s3.js; route=s3-post-policy-test; WIP: desks-get-markup.js: cell by cell updates`
 
 const _ianaTimeZone = 'Asia/Kuala_Lumpur'
