5edbfba256d387c78b51824d9bbfcffb2fce5d8c
jerng-machines <sbx_user1051@169.254.31.245>
2023-06-20 19:43:18 +0000

    comments : classification

--- modules/framework-description-logger.js
+++ modules/framework-description-logger.js
@@ -59,8 +59,8 @@ if (conf.frameworkDescriptionLogging.length) {
                     '\n' + e1 + '\n' + e1 +
                     ' /* (' +
                     (
-                        (result = err.stack.match(/\n.*\n.*at (.*)\n?/)) ?
-                        result[2] :
+                        (result = err.stack.match(/\n.*\n.*\n.at (.*)\n?/)) ?
+                        result[1] :
                         err.stack
                     ) +
                     ')\n' + e1 + '  * ' +
