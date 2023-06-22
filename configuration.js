6b41b775bd6c9e25abcaaaf6124adb0991d2453b
jerng-machines <sbx_user1051@169.254.56.205>
2023-06-22 10:38:10 +0000

    AWS Lambda layer - for (git) on (ARM) notes

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
 'use strict'
 // Dev: easy to find and edit
 const _gitCommit = 1
-const _gitCommitMessage = `refactoring : [ /var/task/index.js ]` // mark.js x frameworkDescriptionLogger?
+const _gitCommitMessage = 'AWS Lambda layer - for (git) on (ARM) notes'//`refactoring : [ /var/task/index.js ]` // mark.js x frameworkDescriptionLogger?
 
 const _ianaTimeZone = 'Asia/Kuala_Lumpur'
 const _dateTimeFormatBcp47Tag = 'sv'
