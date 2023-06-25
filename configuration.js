59f84e1df97020c7e9a65636f59f5a37f92f4930
jerng-machines <sbx_user1051@169.254.42.237>
2023-06-25 06:11:26 +0000

    frameworkDescriptionLogger can autostart

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
 'use strict'
 // Dev: easy to find and edit
 const _gitCommit = 1
-const _gitCommitMessage = `refactoring : [ /var/task/index.js ]` // mark.js x frameworkDescriptionLogger?
+const _gitCommitMessage = 'frameworkDescriptionLogger can autostart'//`refactoring : [ /var/task/index.js ]` // mark.js x frameworkDescriptionLogger?
 
 const _ianaTimeZone = 'Asia/Kuala_Lumpur'
 const _dateTimeFormatBcp47Tag = 'sv'
