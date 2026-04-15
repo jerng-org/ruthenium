6f6627867c815e73430cfc7bf57ae551cff393cd
jerng-machines <sbx_user1051@169.254.33.141>
2026-04-15 20:25:40 +0000

    normalising resource ontology : route=virtual&type=desk-cells&desk-schema-name=

--- tasks/virtual/forms-get/markup-delete-desk-schema.js
+++ tasks/virtual/forms-get/markup-delete-desk-schema.js
@@ -4,7 +4,7 @@ const rus = require ( '/var/task/modules/r-u-s.js' )
 
 const deleteDeskSchema = async ( data ) => {
     
-    console.error(rus.print.stringify4('debug markup-delete-desk-schema.js', data.RU.request.io))
+    console.error(await rus.print.stringify4('debug markup-delete-desk-schema.js', data.RU.request.io))
     return `
     
         <h2>Desk Schema : <code>delete one</code> </h2>
@@ -14,7 +14,7 @@ const deleteDeskSchema = async ( data ) => {
             Please save this transcription if you wish to reimport it later.
         </h3>
         
-        <pre><code>${ await rus.print.stringify4 ( data.RU.io.deskSchemasGet ) }</code></pre>
+        <pre><code>${ await rus.print.stringify4 ( data.RU.io ) }</code></pre>
         
         `
         
