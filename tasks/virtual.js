26d7f42575c3eeca825a30b234de72692d5aee38
jerng-machines <sbx_user1051@169.254.42.11>
2026-04-15 21:34:21 +0000

    normalising resource ontology : route=virtual&type=desk-cells&desk-schema-name=

--- tasks/virtual.js
+++ tasks/virtual.js
@@ -394,11 +394,13 @@ const virtual = async (data) => {
                             //  DELETE (desk-schemas) ... all or just one?
                             switch (queryScope) {
                                 case ('item') :{
+                                    /*
                                     if (!await deskSchemasGetSuccess(data,data.RU.request.queryStringParameters['thing'][0])){
                                         await rus.http.status404(data)
                                         rus.frameworkDescriptionLogger.callEnds()
                                         return
                                     }
+                                    */
                                     if (!await deskSchemasDeleteSuccess(data,data.RU.request.queryStringParameters['thing'][0])){
                                         await rus.http.status500(data)
                                         rus.frameworkDescriptionLogger.callEnds()
