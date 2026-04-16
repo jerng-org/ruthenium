c1da47f745571c20142f5c70fec09b70ca819585
jerng-machines <sbx_user1051@169.254.44.3>
2026-04-16 15:34:19 +0000

    normalisation of ontology : continued

--- tasks/virtual/forms-get/markup-delete-desk-schema.js
+++ tasks/virtual/forms-get/markup-delete-desk-schema.js
@@ -14,7 +14,7 @@ const deleteDeskSchema = async ( data ) => {
         </h3>
         
         <pre><code>
-            ${ await JSON.stringify4(data.RU.io.deskSchemasDelete.Attributes) }
+            ${ await JSON.stringify4(data.RU.io.deskSchemasDelete.Attributes, null, 4) }
         </code></pre>
         
         `
